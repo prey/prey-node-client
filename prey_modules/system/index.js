c393e5ebb4501df61c7a56afe97717ac7b85b0b6
Tomás Pollak <tomaspollak@gmail.com>
2011-10-27 13:18:21 -0300

    System: Added CPUInfo and NetworkInterfaces methods.

--- prey_modules/system/index.js
+++ prey_modules/system/index.js
@@ -6,6 +6,7 @@
 //////////////////////////////////////////
 
 var sys = require('sys'),
+		os = require('os'),
 		Command = require('../../lib/command'),
 		ActionModule = require('../../core/action_module');
 
@@ -15,9 +16,13 @@ var System = function(){
 	var self = this;
 	this.name = 'system';
 
+	this.data = {};
+
 	this.start = function(){
 
-		console.log(' -- Getting system information...')
+		console.log(' -- Getting system information...');
+		this.get_cpu_info();
+		this.get_network_interfaces();
 		// this.done();
 
 	}
@@ -27,6 +32,46 @@ var System = function(){
 		console.log(' -- Stopping...')
 
 	}
+
+	this.get_cpu_info = function(){
+
+		var cpus = os.cpus();
+
+		var cpu_info = {
+			model: cpus[0].model,
+			speed: cpus[0].speed,
+			cores: cpus.length
+		}
+
+		this.data.cpu_info = cpu_info;
+
+	};
+
+	this.get_network_interfaces = function(){
+
+		this.data.network_interfaces = {};
+		var nics = os.getNetworkInterfaces();
+
+		for(key in nics){
+
+			if(key == 'lo') continue;
+
+			var nic = nics[key];
+			// console.log(nic);
+
+			var hash = {
+				name: key,
+				ip_address: nic[0].address,
+				mac_address: nic[1].address
+			}
+
+			this.data.network_interfaces[key] = hash;
+
+		}
+
+		console.log(this.data);
+
+	};
 
 };
 
