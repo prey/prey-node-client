9acd615bc4545d1d80501762a3af90acb87c35ef
Tomás Pollak <tomaspollak@gmail.com>
2011-10-31 00:26:16 -0300

    Dont output everything on ssl/generate.sh

--- ssl/generate.sh
+++ ssl/generate.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 # generates self-signed certificates for On-Demand TLS Connection
+
+#country_code="US"
+#state_name=""     # ST: State or Province Name
+#locality=""       # L: Locality Name
+#organization=""   # O: Organization Name
+#unit=""           # OU: Organization Unit
+#common_name=""    # CN: Common Name
+
 curr_path=$(dirname $0)
+
+[ ! -f "$curr_path/ca.cnf" ] && echo "ca.cnf file not found" && exit 1
 
 cleanup(){
 	rm -f "$curr_path/*.key"
@@ -14,11 +24,14 @@ generate_key(){
 }
 
 signed_by_ca(){
+
+	# local ca_subj="/C=${country_code}/ST=${state}/L=${locality}/O=${organization}/OU=${unit}/CN=${common_name}"
+
 	echo "Generating Certificate Authority certificate"
-	openssl req -new -x509 -config "$curr_path/ca.cnf" -keyout "$curr_path/ca.key" -out "$curr_path/ca.cert"
+	openssl req -new -x509 -config "$curr_path/ca.cnf" -keyout "$curr_path/ca.key" -out "$curr_path/ca.cert" > /dev/null
 
 	echo "Creating a Certificate Signing Request for the key..."
-	openssl req -new -config "$curr_path/ssl.cnf" -key "$curr_path/ssl.key" -out "$curr_path/ssl.csr"
+	openssl req -new -config "$curr_path/ssl.cnf" -key "$curr_path/ssl.key" -out "$curr_path/ssl.csr" > /dev/null
 
 	echo "Generating new certificate..."
 	openssl x509 -req \
@@ -30,17 +43,17 @@ signed_by_ca(){
 		-CAcreateserial \
 		-out "$curr_path/ssl.cert"
 
-	echo "Verifying certificate..."
-	 openssl verify -CAfile "$curr_path/ca.cert" "$curr_path/ssl.cert"
+	# echo "Verifying certificate..."
+	# openssl verify -CAfile "$curr_path/ca.cert" "$curr_path/ssl.cert" > /dev/null
 }
 
 self_signed(){
 
 	echo "Generating new certificate..."
-	openssl req -new -config "$curr_path/ssl.cnf" -key "$curr_path/ssl.key" -x509 -days 999 -out "$curr_path/ssl.cert"
+	openssl req -new -config "$curr_path/ssl.cnf" -key "$curr_path/ssl.key" -x509 -days 999 -out "$curr_path/ssl.cert" > /dev/null
 
 	echo "Verifying certificate..."
-	openssl verify "$curr_path/ssl.cert"
+	openssl verify "$curr_path/ssl.cert" > /dev/null
 
 }
 
