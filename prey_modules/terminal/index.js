443f02434c466db4026b55256556b0e0d59d51a1
Tomás Pollak <tomaspollak@gmail.com>
2011-11-01 20:20:37 -0300

    Added terminal module, also thanks to Tunnel.js

---
+++ prey_modules/terminal/index.js
@@ -0,0 +1,101 @@
+//////////////////////////////////////////
+// Prey JS Terminal Module
+// (c) 2011, Fork Ltd. -- forkhq.com
+// Written by Tomás Pollak
+// Licensed under the GPLv3
+//////////////////////////////////////////
+
+var base = require('../../core/base'),
+		sys = require('sys'),
+		Tunnel = require('../../core/tunnel'),
+		Command = require('../../lib/command'),
+		ActionModule = require('../../core/action_module'),
+		os_functions = require('./platform/' + base.os_name);
+
+var Terminal = function(){
+
+	ActionModule.call(this);
+	var self = this;
+	this.name = 'terminal';
+
+	this.options = {
+		ssh_port: 22,
+		tunnel_host: 'kiwi',
+		tunnel_port: 9999
+	}
+
+	this.start = function(){
+
+		this.tunnel = new Tunnel(this.options.ssh_port, this.options.tunnel_host, this.options.tunnel_port);
+
+		this.tunnel.on('opened', function(){
+
+			self.log("Tunnel is open!");
+
+			os_functions.ssh_server_running(function(running){
+
+				if(!running) self.start_ssh_server();
+
+			});
+
+		});
+
+		this.tunnel.on('closed', function(){
+
+			self.log("Tunnel closed!");
+
+			if(self.remote_terminal_command) // means we launched the ssh server
+				self.shop_ssh_server();
+
+		});
+
+	}
+
+	this.start_ssh_server = function(){
+
+
+		console.log("Starting SSH server!");
+
+		var ssh_cmd = os_functions.ssh_server_command;
+
+		this.remote_terminal_command = new Command(ssh_cmd);
+
+		if (self.remote_terminal_command.is_running())
+			self.log("SSH server is running!");
+
+		self.remote_terminal_command.on('exit', function(code){
+			self.log("SSH server not running.");
+			if(self.tunnel.is_open()) self.tunnel.close();
+			// self.done();
+		});
+
+		self.remote_terminal_command.on('error', function(e){
+			self.log('SSH server closed abruptly with status : ' + e.code);
+			// console.log(e);
+		});
+
+//		self.remote_terminal_command.on('return', function(output){
+//			console.log(output);
+//		});
+
+	};
+
+	this.stop_ssh_server = function(){
+
+		// todo
+
+	};
+
+	this.stop = function(){
+
+		if(this.tunnel.is_open())
+			this.tunnel.close(); // will trigger remote desktop command to stop
+		else if(this.remote_terminal_command)
+			this.stop_ssh_server();
+
+	}
+
+};
+
+sys.inherits(Terminal, ActionModule);
+module.exports = new Terminal();
---
+++ prey_modules/terminal/platform/linux/index.js
@@ -0,0 +1,21 @@
+//////////////////////////////////////////
+// Prey JS Terminal Module Linux Functions
+// (c) 2011 - Fork Ltd.
+// by Tomas Pollak - http://forkhq.com
+// GPLv3 Licensed
+//////////////////////////////////////////
+
+var base = require(process.env.ROOT_PATH + "/core/base");
+
+exports.ssh_server_running = function(){
+
+	base.helpers.run_cmd('ps aux | grep sshd > /dev/null && echo 1', function(output){
+
+		if(output == null || output == "")
+			return false;
+		else
+			return true;
+
+	});
+
+}
