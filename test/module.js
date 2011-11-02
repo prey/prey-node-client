b2d5a6f45afa873e62ba92e33fc08054dcf1d167
Tomás Pollak <tomaspollak@gmail.com>
2011-11-02 20:21:39 -0300

    test/module.js should run() module, rather than start() it

--- test/module.js
+++ test/module.js
@@ -1,10 +1,15 @@
 var module_name = process.argv[2];
 
+if(module_name == null || module_name == ""){
+	console.log("No module name given");
+	process.exit(1);
+}
+
 console.log("loading " + module_name);
 
 var module = require(__dirname + '/../prey_modules/' + module_name);
 
-module.start();
+module.run();
 
 process.on('SIGINT', function () {
 	log(' >> Got Ctrl-C!');
