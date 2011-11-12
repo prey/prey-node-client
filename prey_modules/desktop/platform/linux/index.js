2322800c8f0aa3d6e35af35bdc39849010c0756e
Tomás Pollak <tomaspollak@gmail.com>
2011-11-12 19:03:50 -0300

    Desktop: Added TightFileTransfer to x11vnc's arguments

--- prey_modules/desktop/platform/linux/index.js
+++ prey_modules/desktop/platform/linux/index.js
@@ -7,7 +7,8 @@
 
 exports.vnc_command = function(options){
 
-	var str = 'x11vnc -ncache 10 -ncache_cr -localhost -noxdamage -forever -notruecolor';
+	var str = 'x11vnc -ncache 10 -ncache_cr -localhost -forever'; // main
+	str += ' -tightfilexfer -noxdamage -notruecolor'; // display
 	str += ' -scale 1/' + options.desktop_scale;
 	// if(options.vnc_pass != '') str += ' -passwd "' + options.vnc_pass + '"';
 	if(options.view_only) str += ' -viewonly';
