65121e1ed2650adc352859a7550d1e6e9ab3d588
Tomás Pollak <tomaspollak@gmail.com>
2011-11-22 13:49:28 -0300

    Added a few things to node-gstreamer. Handles desktop sources

--- test/gstreamer.js
+++ test/gstreamer.js
@@ -9,10 +9,10 @@ if(typeof command == 'undefined')
 
 if(command == 'frame'){
 
-GStreamer.captureFrame('frame.jpg', {width: 320, height: 240}, function(file){
+GStreamer.captureFrame('desktop', __dirname + '/frame.jpg', {width: 320, height: 240}, function(file){
 	if(file){
 		console.log("Picture taken: " + file);
-		fs.unlink(file);
+		// fs.unlink(file);
 	} else {
 		console.log("Picture NOT taken. Camera in use?");
 	}
@@ -20,30 +20,30 @@ GStreamer.captureFrame('frame.jpg', {width: 320, height: 240}, function(file){
 
 } else if (command == 'sound'){
 
-var file = 'C:/test/prey.node/test/atoms.mp3';
+var file = process.argv[3];
 
 GStreamer.playSound(file, function(file_played){
 	if(file_played)
 		console.log('Sound played: ' + file_played);
-      else
+	else
 		console.log('File not played!');
 });
 
 } else if(command == 'video'){
 
-GStreamer.streamVideo({dest: 'localhost:9000', encoder: 'vp8enc'}, function(success){
+GStreamer.streamVideo('videotestsrc', {buffers: 100, dest: 'file.flv', encoder: 'ffenc_flv'}, function(success){
 	if(success)
 		console.log('Stream is running!');
-      else
+	else
 		console.log('Stream NOT running!');
 });
 
 } else if(command == 'audiovideo'){
 
-GStreamer.streamAudioVideo({dest: 'file.webm', encoder: 'vp8enc'}, function(success){
+GStreamer.streamVideoWithAudio('webcam', {dest: 'localhost:9000', encoder: 'vp8enc'}, function(success){
 	if(success)
 		console.log('Stream is running!');
-      else
+	else
 		console.log('Stream NOT running!');
 });
 
