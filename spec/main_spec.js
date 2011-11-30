d0add358c02ad6fb765d8a1e5d07acdb5cbfa3b8
Tomás Pollak <tomaspollak@gmail.com>
2011-11-30 18:24:57 -0300

    Added spec test file. Lots of work to be done here.

---
+++ spec/main_spec.js
@@ -0,0 +1,13 @@
+var Main = require('../core/main');
+
+describe('main', function() {
+
+	describe('whatever', function() {
+
+		it('should pass', function() {
+	 		expect(1).toBe(1);
+		});
+
+	});
+
+});
