af55934a23af9ebc77ffa47d63d66d1c46208c95
Tomás Pollak <tomaspollak@gmail.com>
2012-01-02 22:29:56 -0300

    Added provider test script

---
+++ test/provider.js
@@ -0,0 +1,25 @@
+var util = require('util'),
+	common = require('./../lib/prey/common').load_config();
+
+var provider_name = process.argv[2];
+
+if(provider_name == null || provider_name == ""){
+	console.log("No provider name given");
+	process.exit(1);
+}
+
+console.log("Loading " + provider_name);
+
+var provider = require(__dirname + '/../lib/prey/providers/' + provider_name);
+
+provider.getters.forEach(function(data){
+
+	console.log("Requesting " + data)
+
+	provider.get(data, function(result){
+
+		console.log(" - Got " + data + " -> " + util.inspect(result));
+
+	});
+
+});
