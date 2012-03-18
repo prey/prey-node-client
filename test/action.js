1e525673bd783e4a8b2757d32387517c58e68309
Tomás Pollak <tomaspollak@gmail.com>
2012-03-18 02:34:55 -0400

    Added download info about AMSTracker (motion detector). Fixed test/action.

--- test/action.js
+++ test/action.js
@@ -11,16 +11,22 @@ console.log("loading " + module_name);
 
 var mod = require(__dirname + '/../lib/prey/plugins/actions/' + module_name);
 
-mod.start({}, function(err){
+var instance = mod.start({}, function(err){
 	console.log(err);
 });
 
-(mod.events || []).forEach(function(ev){
-	mod.on(ev), function(data){
-		console.log("Event triggered " + ev);
-		if(data) console.log(data);
-	}
-})
+if(instance && mod.events){
+
+	mod.events.forEach(function(ev){
+
+		instance.on(ev, function(data){
+			console.log("Event triggered " + ev);
+			if(data) console.log(data);
+		});
+
+	});
+	
+}
 
 process.on('SIGINT', function () {
 	console.log(' >> Got Ctrl-C!');
