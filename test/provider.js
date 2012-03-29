b37e0f9d3e4a22dc1b4a88f41a80ea53e438a23c
Tomás Pollak <tomaspollak@gmail.com>
2012-03-29 00:13:50 -0400

    Allow testing all providers on test/providers.js.

--- test/provider.js
+++ test/provider.js
@@ -3,23 +3,41 @@ var util = require('util'),
 
 var provider_name = process.argv[2];
 
-if(provider_name == null || provider_name == ""){
-	console.log("No provider name given");
-	process.exit(1);
-}
+var load_provider = function(provider_name){
 
-console.log("Loading " + provider_name);
+	console.log("Loading " + provider_name);
 
-var provider = require(__dirname + '/../lib/prey/plugins/providers/' + provider_name);
+	var provider = require(__dirname + '/../lib/prey/plugins/providers/' + provider_name);
 
-provider.getters.forEach(function(data){
+	provider.getters.forEach(function(data){
 
-	console.log("Requesting " + data)
+		console.log("Requesting " + data)
 
-	provider.get(data, function(err, result){
+		provider.get(data, function(err, result){
 
-		console.log(" - Got " + data + " -> " + util.inspect(result));
+			if(err) console.log(' !! Error for ' + data + ' -> ' + err.toString())
+			else console.log(" -- Got " + data + " -> " + util.inspect(result));
+
+		});
 
 	});
 
-});
+
+}
+
+if(provider_name == null || provider_name == ""){
+	console.log("No provider name given");
+	process.exit(1);
+} else if(provider_name == 'all'){
+	
+	require('fs').readdir(__dirname + '/../lib/prey/plugins/providers/', function(err, list){
+
+		list.forEach(function(p){
+			load_provider(p);
+		})
+
+	})
+	
+} else {
+	load_provider(provider_name);
+}
