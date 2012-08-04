18ffdf57c049fe068063b8789f41b8cdab8187db
Tomás Pollak <tomaspollak@gmail.com>
2012-08-04 05:44:11 -0400

    Moved some stuff from cli_spec into common_spec.

--- spec/common_spec.js
+++ spec/common_spec.js
@@ -1,3 +1,26 @@
+var common = require('./../lib/prey/common')
+
 describe('common', function(){
-	
+
+	it('should set Preys ROOT_PATH in process.env', function(){
+		true.should.equal(true);
+	});
+
+	describe('config path', function(){
+
+		it('should default to OS config path', function(){
+
+		})
+
+		it('should check if path is writable', function(){
+
+		})
+
+		it('should check if config file is readable', function(){
+
+		})
+
+	})
+
+
 })
