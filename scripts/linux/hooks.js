56ba43465bc6a959fafa745a8f1da4e73716876a
ritchie turner <ritchie@async.cl>
2012-10-02 08:35:58 -0300

    untabify and lint

--- scripts/linux/hooks.js
+++ scripts/linux/hooks.js
@@ -1,32 +1,34 @@
+"use strict";
+
 var fs = require('fs'),
-		path = require('path'),
-		exec = require('child_process').exec,
-		system = require('./../../lib/prey/plugins/providers/system');
+    path = require('path'),
+    exec = require('child_process').exec,
+    system = require('./../../lib/prey/plugins/providers/system');
 
 var prey_bin = exports.prey_bin = '/usr/local/bin/prey';
 
 var init_script_name = 'prey-trigger',
-		common_initd_path = '/etc/init.d';
+    common_initd_path = '/etc/init.d';
 
 var weird_initd_paths = {
-	redhat: '/etc/rc.d/init.d',
-	arch: '/etc/rc.d'
+  redhat: '/etc/rc.d/init.d',
+  arch: '/etc/rc.d'
 };
 
 var initd_commands = {
-	debian: {
-		load: 'update-rc.d $1 defaults',
-		unload: 'update-rc.d -f $1 remove',
-	},
-	redhat: {
-		load: 'chkconfig $1 on',
-		unload: 'chkconfig $1 off'
-	},
-	suse: {
-		load: 'chkconfig --add $1',
-		unload: 'chkconfig --del $1'
-	}
-}
+  debian: {
+    load: 'update-rc.d $1 defaults',
+    unload: 'update-rc.d -f $1 remove'
+  },
+  redhat: {
+    load: 'chkconfig $1 on',
+    unload: 'chkconfig $1 off'
+  },
+  suse: {
+    load: 'chkconfig --add $1',
+    unload: 'chkconfig --del $1'
+  }
+};
 
 initd_commands.ubuntu = initd_commands.debian;
 initd_commands.fedora = initd_commands.redhat;
@@ -37,44 +39,44 @@ initd_commands.fedora = initd_commands.redhat;
 
 var get_init_script_path = function(distro){
 
-	var initd_path = weird_initd_paths[distro] || common_initd_path;
-	return path.join(initd_path, init_script_name);
+  var initd_path = weird_initd_paths[distro] || common_initd_path;
+  return path.join(initd_path, init_script_name);
 
 };
 
 var copy_init_script = function(distro, callback){
 
-	var full_path = get_init_script_path(distro);
+  var full_path = get_init_script_path(distro);
 
-	fs.exists(full_path, function(exists){
-		if(exists) return callback(new Error("File already exists!"))
+  fs.exists(full_path, function(exists){
+    if(exists) return callback(new Error("File already exists!"));
 
-		var template = fs.readFileSync(path.resolve(__dirname + "/" + init_script_name));
-		var data = template.toString().replace('{{prey_bin}}', prey_bin);
+    var template = fs.readFileSync(path.resolve(__dirname + "/" + init_script_name));
+    var data = template.toString().replace('{{prey_bin}}', prey_bin);
 
-		if(data === template.toString())
-			return callback(new Error("Unable to replace template variables!"));
+    if(data === template.toString())
+      return callback(new Error("Unable to replace template variables!"));
 
-		fs.writeFile(full_path, data, callback);
+    fs.writeFile(full_path, data, callback);
 
-	});
+  });
 
 };
 
 var remove_init_script = function(distro, callback){
-	var file = get_init_script_path(distro);
-	fs.unlink(file, callback);
-}
+  var file = get_init_script_path(distro);
+  fs.unlink(file, callback);
+};
 
 var load_init_script = function(distro, callback){
-	var command = initd_commands[distro].load.replace('$1', init_script_name);
-	exec(command, callback);
-}
+  var command = initd_commands[distro].load.replace('$1', init_script_name);
+  exec(command, callback);
+};
 
 var unload_init_script = function(distro, callback){
-	var command = initd_commands[distro].unload.replace('$1', init_script_name);
-	exec(command, callback);
-}
+  var command = initd_commands[distro].unload.replace('$1', init_script_name);
+  exec(command, callback);
+};
 
 /////////////////////////////////////////////////
 // hooks
@@ -82,37 +84,38 @@ var unload_init_script = function(distro, callback){
 
 exports.post_install = function(callback){
 
-	system.get('os_name', function(err, name){
+  system.get('os_name', function(err, name){
 
-		var distro = name.toLowerCase();
-		copy_init_script(distro, function(err){
+    var distro = name.toLowerCase();
+    copy_init_script(distro, function(err){
 
-			if(err) return callback(err);
-			load_init_script(distro, callback)
+      if(err) return callback(err);
+      load_init_script(distro, callback);
 
-		})
+    });
 
-	})
+  });
 
-}
+};
 
 exports.pre_uninstall = function(callback){
 
-	system.get('os_name', function(err, name){
+  system.get('os_name', function(err, name){
 
-		var distro = name.toLowerCase();
-		unload_init_script(distro, function(err){
+    var distro = name.toLowerCase();
+    unload_init_script(distro, function(err){
 
-			if(err) return callback(err);
-			remove_init_script(distro, function(err){
+      if(err) return callback(err);
+      remove_init_script(distro, function(err){
 
-				if(!err || err.code == 'ENOENT') callback();
-				else callback(err);
+        if(!err || err.code === 'ENOENT') callback();
+        else callback(err);
 
-			});
+      });
 
-		})
+    });
 
-	})
+  });
 
-}
+};
+
