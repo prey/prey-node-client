f4e0647b78073881c2cbd8e1fa321c89c13108ca
ritchie <ritchie@async.cl>
2012-11-08 05:40:36 -0300

    add async.waterfall to control post_install

--- scripts/windows/hooks.js
+++ scripts/windows/hooks.js
@@ -1,86 +1,149 @@
 "use strict";
 
 var 
-    exec = require('child_process').exec,
-    common = _ns('common');
+  async = require('async'),
+  exec = require('child_process').exec,
+  common = _ns('common'),
+  service_name = "PreyCronService.exe";
 
-var service_exists = function(callback) {
-    var cmd = 'sc qc prey';
-    exec(cmd,function(err,stdout) {
-        if(err) return callback(_error(err));
+/**
+ * Callback a bool if the service is installed.
+ **/
+ var service_exists = function(callback) {
+  var cmd = 'sc qc prey';
+  exec(cmd,function(err,stdout) {
+    if(err) return callback(_error(err));
 
-        callback(null,stdout.indexOf("SUCCESS") !== -1);
-    });
+    callback(null,stdout.indexOf("SUCCESS") !== -1);
+  });
 };
 
-var service_delete = function(callback) {
-    var cmd = 'sc delete prey';
-    exec(cmd,function(err,stdout) {
-        if(err) return callback(_error(err));
+/**
+ * Get the pid of the running service else null.
+ **/
+ var service_running = function(callback) {
+  var cmd = 'tasklist /nh /fi "imagename eq '+service_name+'"';
+  exec(cmd, function(err, stdout){
+    if (err) return callback(_error("!:"+cmd,err));
 
-        callback(null,stdout.indexOf("SUCCESS") !== -1);
-    });
+    if (stdout.toString().indexOf(service_name) === -1) {
+      return callback(null,null); //service not running
+    }
+
+    var cols = stdout.split(/\s+/),
+    pid  = cols[2];
+    callback(null,parseInt(pid));
+  });
 };
 
-var service_create = function(callback) {
-    var binPath = common.root_path + 'bin/PreyCronService.exe',
-        cmd = 'sc create prey binPath= '+binPath;
+/**
+ * Kill the running service process, note this is not the same as service_delete.
+ **/
+var service_kill = function(pid,callback) {
+  var cmd = 'taskkill /f /pid ' + pid;
+  exec(cmd, function(err, stdout){
+    if (err) return callback(_error("!:"+cmd,err));
 
-    exec(cmd,function(err,stdout) {
-        if(err) return callback(_error(err));
+    callback(null,stdout.indexOf("SUCCESS") !== -1);
+  });
+};
 
-        callback(null,stdout.indexOf("SUCCESS") !== -1);
-    });
+/**
+ * Delete the service from the service registry.
+ **/
+ var service_delete = function(callback) {
+  var cmd = 'sc delete prey';
+  exec(cmd,function(err,stdout) {
+    if(err) return callback(_error(err));
+
+    callback(null,stdout.indexOf("SUCCESS") !== -1);
+  });
+};
+
+/**
+ * Create a service, by providing the path to the executable. 
+ **/
+ var service_create = function(callback) {
+  var binPath = common.root_path + 'bin/PreyCronService.exe',
+  cmd = 'sc create prey binPath= '+binPath;
+
+  exec(cmd,function(err,stdout) {
+    if(err) return callback(_error(err));
+
+    callback(null,stdout.indexOf("SUCCESS") !== -1);
+  });
 };
 
 /**
  * Callback the service PID if all is well, else null.
  **/
-var service_start = function(callback) {
-    var cmd = 'sc start prey';
-    exec(cmd,function(err,stdout) {
-        if(err) return callback(_error("!:"+cmd,err));
+ var service_start = function(callback) {
+  var cmd = 'sc start prey';
+  exec(cmd,function(err,stdout) {
+    if(err) return callback(_error("!:"+cmd,err));
 
-        var m = stdout.match(/PID\s+?:\s([0-9]+?)\s/);
+    var m = stdout.match(/PID\s+?:\s([0-9]+?)\s/);
 
-        if (!m) return callback(null,null);
+    if (!m) return callback(null,null);
 
-        callback(null,m[1]);
-    });  
+    callback(null,m[1]);
+  });  
 };
 
-var create_and_start = function(callback) {
-    service_create(function(err,success) {
-      if(err) return callback(_error(err));
-      if (success) {
-          service_start(function(err,success) {
-            if(err) return callback(_error(err));
+exports.post_install = function(callback) {
+  async.waterfall([
+    service_exists,
 
-            callback(null,success);
+    // remove running instance if it exists ...
+    function(exists,cb) {
+      if (!exists) return cb(null,exists);
+
+      service_running(function(err,pid) {
+        if (err) return cb(err);
+        if (!pid) return cb(null,exists);
+
+        service_kill(pid,function(err,success) {
+          if (err) return cb(err);
+          if (!success) return cb("Can't kill service process "+pid);
+          cb(null,exists);
         });
-      }
-    });
-};
+      });
+    },
 
-exports.post_install = function(callback){
-    service_exists(function(err,exists) {
-        if(err) return callback(_error(err));
-    
-        if (exists) {
-          service_delete(function(err,success) {
-            if(err) return callback(_error(err));
-            
-            create_and_start(function(err,success) {
-                callback(null);
-            });
-          });
-        }
+    // now that running instance is deleted remove service itself ...
+    function(exists,cb) {
+      if (!exists) cb(null);
 
-    });
+      service_delete(function(err,success) {
+        if (err) return cb(err);
+        if (!success) return cb("Can't delete existing service");
+        cb(null);
+      });
+    },
+
+    service_create,
+
+    // service create now start it ...
+    function(success,cb) {
+      if (!success) return cb('service creation unsuccessful');
+
+      service_start(function(err,pid) {
+        if (err) return cb(err);
+        cb(null,pid);
+      });
+    }
+
+    ],
+  function(err,pid) {
+    if (err) return callback(_error(err));
+
+    // new service should have been created and instantiated, return it's pid
+    callback(null,pid);
+  });
 };
 
 exports.pre_uninstall = function(callback){
 	callback();
-}
+};
 
 
