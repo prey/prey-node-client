7361ac6657252b5453f8d7ab0d487a12fe6186f0
ritchie <ritchie@async.cl>
2012-11-08 07:56:22 -0300

    move configurator into own module

---
+++ configurator/linux/index.js
@@ -0,0 +1,125 @@
+"use strict";
+
+var fs = require('fs'),
+    path = require('path'),
+    exec = require('child_process').exec;
+
+var get_os_name = function(callback){
+  var cmd = 'lsb_release -i';
+  exec(cmd, function(err, stdout){
+    if(err) return callback(_error("!:" + cmd,err));
+    
+    callback(null,stdout.toString().split(":")[1].trim());  
+  });
+};
+
+var prey_bin = exports.prey_bin = '/usr/local/bin/prey';
+var etc_dir = exports.etc_dir = '/etc/prey';
+
+var init_script_name = 'prey-trigger',
+    common_initd_path = '/etc/init.d';
+
+var weird_initd_paths = {
+  redhat: '/etc/rc.d/init.d',
+  arch: '/etc/rc.d'
+};
+
+var initd_commands = {
+  debian: {
+    load: 'update-rc.d $1 defaults',
+    unload: 'update-rc.d -f $1 remove'
+  },
+  redhat: {
+    load: 'chkconfig $1 on',
+    unload: 'chkconfig $1 off'
+  },
+  suse: {
+    load: 'chkconfig --add $1',
+    unload: 'chkconfig --del $1'
+  }
+};
+
+initd_commands.ubuntu = initd_commands.debian;
+initd_commands.linuxmint = initd_commands.debian;
+initd_commands.fedora = initd_commands.redhat;
+
+/////////////////////////////////////////////////
+// helpers
+/////////////////////////////////////////////////
+
+var get_init_script_path = function(distro){
+  var initd_path = weird_initd_paths[distro] || common_initd_path;
+  return path.join(initd_path, init_script_name);
+};
+
+var copy_init_script = function(distro, callback){
+  var full_path = get_init_script_path(distro);
+
+  fs.exists(full_path, function(exists){
+    if (exists) { 
+      _tr('unlinking '+full_path);
+      fs.unlink(full_path);
+    }
+
+
+    var template = fs.readFileSync(path.resolve(__dirname + "/" + init_script_name));
+    var data = template.toString().replace('{{prey_bin}}', prey_bin);
+
+    if(data === template.toString())
+      return callback(new Error("Unable to replace template variables!"));
+
+    console.log('copying to '+full_path);
+    fs.writeFile(full_path, data, callback);
+
+  });
+};
+
+var remove_init_script = function(distro, callback){
+  var file = get_init_script_path(distro);
+  fs.unlink(file, callback);
+};
+
+var load_init_script = function(distro, callback){
+  console.log('distro is = '+distro);
+  var command = initd_commands[distro].load.replace('$1', init_script_name);
+  console.log('command is '+command);
+  exec(command, callback);
+};
+
+var unload_init_script = function(distro, callback){
+  var command = initd_commands[distro].unload.replace('$1', init_script_name);
+  exec(command, callback);
+};
+
+/////////////////////////////////////////////////
+// hooks
+/////////////////////////////////////////////////
+
+exports.post_install = function(callback) {
+  console.log('in post_install');
+  get_os_name(function(err, name) {
+    var distro = name.toLowerCase();
+    console.log('distro is '+distro);
+    copy_init_script(distro, function(err){
+      if(err) return callback(err);
+      
+      load_init_script(distro, callback);
+    });
+  });
+};
+
+exports.pre_uninstall = function(callback){
+  get_os_name(function(err, name){
+    var distro = name.toLowerCase();
+    unload_init_script(distro, function(err){
+      if(err) return callback(err);
+      
+      remove_init_script(distro, function(err){
+        if(!err || err.code === 'ENOENT') callback();
+        
+        else callback(err);
+      });
+    });
+  });
+};
+
---
+++ configurator/windows/index.js
@@ -0,0 +1,158 @@
+"use strict";
+
+var 
+  async = require('async'),
+  exec = require('child_process').exec,
+  common = _ns('common'),
+  service_name = "PreyCronService.exe";
+
+/**
+ * Callback a bool if the service is installed.
+ **/
+ var service_exists = function(callback) {
+  var cmd = 'sc qc prey';
+  exec(cmd,function(err,stdout) {
+    if(err) return callback(_error(err));
+
+    callback(null,stdout.indexOf("SUCCESS") !== -1);
+  });
+};
+
+/**
+ * Get the pid of the running service else null.
+ **/
+ var service_running = function(callback) {
+  var cmd = 'tasklist /nh /fi "imagename eq '+service_name+'"';
+  exec(cmd, function(err, stdout){
+    if (err) return callback(_error("!:"+cmd,err));
+
+    if (stdout.toString().indexOf(service_name) === -1) {
+      return callback(null,null); //service not running
+    }
+
+    var cols = stdout.split(/\s+/),
+    pid  = cols[2];
+    callback(null,parseInt(pid));
+  });
+};
+
+/**
+ * Kill the running service process, note this is not the same as service_delete.
+ **/
+var service_kill = function(pid,callback) {
+  var cmd = 'taskkill /f /pid ' + pid;
+  exec(cmd, function(err, stdout){
+    if (err) return callback(_error("!:"+cmd,err));
+
+    callback(null,stdout.indexOf("SUCCESS") !== -1);
+  });
+};
+
+/**
+ * Delete the service from the service registry.
+ **/
+ var service_delete = function(callback) {
+  var cmd = 'sc delete prey';
+  exec(cmd,function(err,stdout) {
+    if(err) return callback(_error(err));
+
+    callback(null,stdout.indexOf("SUCCESS") !== -1);
+  });
+};
+
+/**
+ * Create a service, by providing the path to the executable. 
+ **/
+ var service_create = function(callback) {
+  var binPath = common.root_path + 'bin/PreyCronService.exe',
+  cmd = 'sc create prey binPath= '+binPath;
+
+  exec(cmd,function(err,stdout) {
+    if(err) return callback(_error(err));
+
+    callback(null,stdout.indexOf("SUCCESS") !== -1);
+  });
+};
+
+/**
+ * Callback the service PID if all is well, else null.
+ **/
+ var service_start = function(callback) {
+  var cmd = 'sc start prey';
+  exec(cmd,function(err,stdout) {
+    if(err) return callback(_error("!:"+cmd,err));
+
+    var m = stdout.match(/PID\s+?:\s([0-9]+?)\s/);
+
+    if (!m) return callback(null,null);
+
+    callback(null,m[1]);
+  });  
+};
+
+var remove_existing = function(callback) {
+ async.waterfall([
+    service_exists,
+
+    // remove running instance if it exists ...
+    function(exists,cb) {
+      if (!exists) return cb(null,exists);
+
+      service_running(function(err,pid) {
+        if (err) return cb(err);
+        if (!pid) return cb(null,exists);
+
+        service_kill(pid,function(err,success) {
+          if (err) return cb(err);
+          if (!success) return cb("Can't kill service process "+pid);
+          cb(null,exists);
+        });
+      });
+    },
+
+    // now that running instance is deleted remove service itself ...
+    function(exists,cb) {
+      if (!exists) cb(null);
+
+      service_delete(function(err,success) {
+        if (err) return cb(err);
+        if (!success) return cb("Can't delete existing service");
+        cb(null);
+      });
+    }
+    ],
+  function(err) {
+    if (err) return callback(_error(err));
+    callback(null);
+  });
+};
+
+exports.post_install = function(callback) {
+  async.waterfall([
+    remove_existing,
+    service_create,
+
+    // start it, if creation successful ...
+    function(success,cb) {
+      if (!success) return cb('Service creation unsuccessful');
+
+      service_start(function(err,pid) {
+        if (err) return cb(err);
+        cb(null,pid);
+      });
+    }
+
+    ],
+  function(err,pid) {
+    if (err) return callback(_error(err));
+
+    // new service should have been created and instantiated, return it's pid
+    callback(null,pid);
+  });
+};
+
+exports.pre_uninstall = function(callback){
+	remove_existing(callback);
+};
+
+
