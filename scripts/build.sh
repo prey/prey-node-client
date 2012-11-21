8d62c9095b08a3f5398ab09c1f266b7bcb815a1f
Tomás Pollak <tomaspollak@gmail.com>
2012-11-21 01:44:19 -0300

    Run specs as part of the build script. Bail if they don't pass.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,37 +1,50 @@
 #!/bin/sh -e
-# This script generates a tarball containing all files, dependencies and the installed node binary.
+########################################################
+# This script generates a tarball containing all files,
+# dependencies and the installed node binary.
+########################################################
 # Output is: dist/prey-$VERSION.tar.gz
+########################################################
 
-SCRIPT_PATH="$(dirname $0)"
-# . "$SCRIPT_PATH/helper_functions.sh"
+run_specs(){
+  npm test
+}
 
-VERSION=$(node -e 'console.log(JSON.parse(require("fs").readFileSync("package.json","utf8")).version)')
-DIST="$(pwd)/dist"
-ROOT="/tmp/prey-build.$$"
-FOLDER="prey-${VERSION}"
-PACKAGE="$FOLDER"
-TARBALL="${PACKAGE}.tar.gz"
-# [ $(uname) == 'Darwin' ] && PACKAGE="prey-mac-$FOLDER" || PACKAGE="prey-linux-$FOLDER"
+build(){
 
-rm -f "${DIST}/${TARBALL}" 2> /dev/null
+  SCRIPT_PATH="$(dirname $0)"
+  VERSION=$(node -e 'console.log(JSON.parse(require("fs").readFileSync("package.json","utf8")).version)')
+  DIST="$(pwd)/dist"
+  ROOT="/tmp/prey-build.$$"
+  FOLDER="prey-${VERSION}"
+  PACKAGE="$FOLDER"
+  TARBALL="${PACKAGE}.tar.gz"
+  # [ $(uname) == 'Darwin' ] && PACKAGE="prey-mac-$FOLDER" || PACKAGE="prey-linux-$FOLDER"
 
-mkdir -p "$ROOT/$FOLDER/node_modules"
-cp -R README.md index.js default.options package.json bin lib scripts test "$ROOT/$FOLDER"
-cd "$ROOT/$FOLDER"
+  rm -f "${DIST}/${TARBALL}" 2> /dev/null
 
-BUNDLE_ONLY=1 npm install --production # > /dev/null
-cd "$ROOT/$FOLDER"
+  mkdir -p "$ROOT/$FOLDER/node_modules"
+  cp -R README.md index.js default.options package.json bin lib scripts test "$ROOT/$FOLDER"
+  cd "$ROOT/$FOLDER"
 
-# echo "Copying node binary..."
-# cp `which node` bin
+  BUNDLE_ONLY=1 npm install --production # > /dev/null
+  cd "$ROOT/$FOLDER"
 
-echo -e "\nBuilding tarball..."
-cd "$ROOT"
-tar czf "$TARBALL" "$FOLDER"
+  # echo "Copying node binary..."
+  # cp `which node` bin
 
-mkdir -p "$DIST"
-cd "$DIST"
-mv "$ROOT/$TARBALL" "$DIST"
-echo "$DIST/$TARBALL"
+  echo -e "\nBuilding tarball..."
+  cd "$ROOT"
+  tar czf "$TARBALL" "$FOLDER"
 
-rm -fr "$ROOT"
+  mkdir -p "$DIST"
+  cd "$DIST"
+  mv "$ROOT/$TARBALL" "$DIST"
+  echo "$DIST/$TARBALL"
+
+  rm -fr "$ROOT"
+
+}
+
+run_specs
+[ $? -eq 0 ] && build
