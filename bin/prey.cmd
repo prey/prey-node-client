8ac6c987f59b1fab3be95642c20f248e48520c28
Tomás Pollak <tomaspollak@gmail.com>
2012-11-22 12:41:19 -0300

    Updated bin/prey.cmd. Fixed conf/windows/service.js

--- bin/prey.cmd
+++ bin/prey.cmd
@@ -1,8 +1,29 @@
-set "cmd=%1"
-shift
+@echo off
 
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe" "%~dp0\..\%cmd" %*
+set path=%~dp0
+
+if "%1" == "config" (
+  set script=\lib\conf\cli.js
+) else if "%1" == "test" (
+  set script=\node_modules\mocha\bin\mocha
+) else (
+  set script=\lib\agent\cli.js
+  if not "%1" == "" (
+    set no_shift=0
+  )
+)
+
+echo %*
+
+if not "%1" == "" (
+  echo %no_shift%
+  if not "%no_shift" == "0" (
+    shift
+ )
+)
+
+@IF EXIST "%path%\node.exe" (
+  "%path%\node.exe" "%path%\..\%script%" %*
 ) ELSE (
-  node "%~dp0\..\%cmd" %*
+  node "%path%\..\%script%" %*
 )
