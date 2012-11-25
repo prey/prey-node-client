76fbaaed22907b5191b548f4f16fcc6c2968b8b5
Tomás Pollak <tomaspollak@gmail.com>
2012-11-24 22:26:52 -0300

    Updated build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/sh -e
 ########################################################
 # This script generates a tarball containing all files,
 # dependencies and the installed node binary.
@@ -32,6 +32,7 @@ build(){
 
   # remove stuff from main tarball
   find . -name "*~" -delete
+  find . -name "__MACOSX" -delete
   rm -f bin/node*
 
   BUNDLE_ONLY=1 npm install --production # > /dev/null
@@ -49,8 +50,19 @@ build(){
 }
 
 zip_file(){
+
+  OS="$1"
   echo -e "\nBuilding ${ZIP} package..."
-  zip -9 -r "$ZIP" "$FOLDER"
+
+  if [ -z "$OS" ]; then
+    zip -9 -r "$ZIP" "$FOLDER" 1> /dev/null
+  elif [ "$OS" = 'windows' ]; then
+  	zip -9 -r "$ZIP" "$FOLDER" -x \*linux* -x \*mac* 1> /dev/null
+  elif [ "$OS" = 'mac' ]; then
+  	zip -9 -r "$ZIP" "$FOLDER" -x \*windows* -x \*.exe -x \*linux* 1> /dev/null
+  elif [ "$OS" = 'linux' ]; then
+  	zip -9 -r "$ZIP" "$FOLDER" -x \*windows* -x \*.exe -x \*mac* 1> /dev/null
+  fi
 
   mv "$ROOT/$ZIP" "$VERSION_PATH"
   echo "$VERSION_PATH/$ZIP"
@@ -67,11 +79,11 @@ pack(){
 
   cp "$CURRENT_PATH/node/current/${NODE_BIN}" "${ROOT}/${FOLDER}/bin"
 
-  if [ "$OS" -ne "windows" ]; then
+  if [ "$OS" = "windows" ]; then
     mv "${ROOT}/${FOLDER}/bin/node.${OS}" "${ROOT}/${FOLDER}/bin/node"
   fi
 
-  zip_file
+  zip_file $OS
   rm -f "${ROOT}/${FOLDER}/bin/node*"
 
 }
