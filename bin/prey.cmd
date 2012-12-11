921fecd64befae4998591b9306d0ed868fe78be4
Rodrigo Fuentealba Cartes <hagar.el.vikingo@gmail.com>
2012-12-11 19:34:52 -0300

    Changed prey executable files to not shift variable arguments, also changed the CLI executable to shift parameters itself.

--- bin/prey.cmd
+++ bin/prey.cmd
@@ -4,19 +4,14 @@ set dir=%~dp0
 
 if "%1" == "config" (
   set script=\lib\conf\cli.js
-  shift
-) else if "%1" == "test" (
+) else if "%1" == "spec" (
   set script=\node_modules\mocha\bin\mocha
-  shift
 ) else (
   set script=\lib\agent\cli.js
 )
 
-REM -- Shift has no affect on the %* batch parameter.
-set args=%1 %2 %3 %4 %5
-
 @IF EXIST "%path%\node.exe" (
-  "%dir%\node.exe" "%dir%\..\%script%" %args%
+  "%dir%\node.exe" "%dir%\..\%script%" %*
 ) ELSE (
-  node "%dir%\..\%script%" %args%
+  node "%dir%\..\%script%" %*
 )
