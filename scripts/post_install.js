77217c349502b5a4af83fbbfc68084f901e6a10c
Tomás Pollak <tomaspollak@gmail.com>
2012-12-13 11:40:32 -0300

    Moved dependencies to root package.json.

--- scripts/post_install.js
+++ scripts/post_install.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
 // this script is executed by npm after an 'npm install prey'
-// it installes dependencies and calls the conf module
-// to set up the config file and execution triggers
+// and calls the conf module to set up config and execution triggers
 
 var fs = require('fs'),
     execFile = require('child_process').execFile,
@@ -12,43 +11,23 @@ var fs = require('fs'),
 
 var post_install = function(){
 
-  execFile(__dirname + '/install_deps.js', function(err){
+  if (process.getuid() != 0) {
+    var msg =  'You are running this script as an unprivileged user';
+       msg +=  '\nso we cannot continue with the system configuration.';
+       msg +=  '\nTo finalize the install process please run: \n\n';
+       msg +=  '  $ sudo scripts/post_install.js';
+    console.log(line + msg + line);
+    process.exit(1);
+  }
+
+   // make sure the executable exists before setting up any triggers
+
+  execFile(prey_bin, args, function(err, stdout, stderr){
+    if (stdout.length > 0) console.log(stdout);
+    if (stderr.length > 0) console.log(stderr);
 
     if (err) return console.log(err);
-    console.log('Dependencies in place!')
-
-    if (process.getuid() != 0) {
-      var msg =  'You are running this script as an unprivileged user';
-         msg +=  '\nso we cannot continue with the system configuration.';
-         msg +=  '\nTo finalize the install process please run: \n\n';
-         msg +=  '  $ sudo scripts/post_install.js';
-      console.log(line + msg + line);
-      process.exit(1);
-    }
-
-    // make sure the executable exists before setting up any triggers
-
-    fs.exists(prey_bin, function(exists){
-
-      if (!exists) {
-        var msg = "We couldn't find the Prey executable in " + prey_bin;
-           msg += "\nIf you installed the package locally, then you need to";
-           msg += "\nlink it to the global path by running\n\n  $ sudo npm link\n";
-           msg += "\nOnce you're done please run this script again.";
-        console.log(line + msg + line);
-        process.exit(1);
-      }
-
-      execFile(prey_bin, args, function(err, stdout, stderr){
-        if (stdout.length > 0) console.log(stdout);
-        if (stderr.length > 0) console.log(stderr);
-
-        if (err) return console.log(err);
-        console.log("System setup successful! You can run Prey now.");
-      });
-
-    });
-
+    console.log("System setup successful! You can run Prey now.");
   });
 
 }
