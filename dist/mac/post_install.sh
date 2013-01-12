3e0fb4e7a32aa3ab6d0a227aad7a768f5910d233
Tomás Pollak <tomaspollak@gmail.com>
2013-01-12 03:55:11 -0300

    Touch/chmod/remove log file in post install scripts.

--- dist/mac/post_install.sh
+++ dist/mac/post_install.sh
@@ -4,14 +4,16 @@ VERSION='0.8.7'
 PREY_USER="prey"
 BASE_PATH="/usr/lib/prey"
 CONFIG_DIR="/etc/prey"
+LOG_FILE="/var/log/prey.log"
 INSTALL_PATH="${BASE_PATH}/versions/${VERSION}"
 BIN_PATH="${INSTALL_PATH}/bin/prey"
 
 bash "$INSTALL_PATH/scripts/create_user.sh" ${PREY_USER}
 mkdir -p $CONFIG_DIR
+touch $LOG_FILE
 
 # set up permissions
-chown -R $PREY_USER: $CONFIG_DIR $BASE_PATH
+chown -R $PREY_USER: $CONFIG_DIR $BASE_PATH $LOG_FILE
 # as prey_user: symlink, write crontab, generate prey.conf
 su ${PREY_USER} -c "$BIN_PATH config activate"
 "$BIN_PATH" config hooks post_install
