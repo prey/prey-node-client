bddc5b1dc61a04acdc07ed4483cd4111dc640797
Tomás Pollak <tomaspollak@gmail.com>
2013-01-14 16:29:47 -0300

    Build script: Remove unneeded stuff from node_modules.

--- scripts/build.sh
+++ scripts/build.sh
@@ -30,12 +30,15 @@ build(){
 
   cd "$ROOT/$FOLDER"
 
+  BUNDLE_ONLY=1 npm install --production # > /dev/null
+
   # remove stuff from main tarball
+  rm -Rf $(find node_modules -name "example*")
+  rm -Rf $(find node_modules -name "test")
+  rm -Rf $(find node_modules -name "*.txt")
   find . -name "*~" -delete
   find . -name "__MACOSX" -delete
   rm -f bin/node*
-
-  BUNDLE_ONLY=1 npm install --production # > /dev/null
 
   cd "$ROOT"
   mkdir -p "$VERSION_PATH"
