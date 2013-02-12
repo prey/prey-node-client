3f07fe1b93c203b7c4946a1653ccb0857f6bca77
Herman Junge <haj@neosource.cl>
2013-02-11 22:14:01 -0300

    Tests for know are long, we will find a way to make them look elegant and compact

--- test/cli_spec.js
+++ test/cli_spec.js
@@ -91,6 +91,10 @@ function testsCLIOptionsPath () {
       should.exists(common.config._values.campfire.token);
       common.config._values.campfire.token.should.eql('USER_TOKEN');      
 
+      // And delete the args and the file
+      process.argv = process.argv.splice(0,2);
+      fs.unlinkSync(tmpConfigFile);
+
       done();
     }
   });
@@ -101,10 +105,81 @@ function testsCLIOptionsPath () {
    * @summary <Write a Sumamry>
    */
   it('should exit when no config file is found', function (done) {
-    
+    // Let's create a `cli` file for testing purposes
+    // We must extract the first line, since it has a shebang `#!`
+    var cliPath = join(__dirname, '..', 'lib', 'agent', 'cli.js');
+    var buffer = fs.readFileSync(cliPath);
+    var newline = ['\r'.charCodeAt(0), '\n'.charCodeAt(0)];
+    var i = 0;
+    while (newline.indexOf(buffer[i]) === -1) { i++; }
+    var restOfTheFile = buffer.slice(i + 1, buffer.length).toString('utf-8');
 
+    var randomNumber = Math.floor(1000000000 * Math.random()).toString()
+    var newFileName = join('/tmp', randomNumber + '_lib_agent_cli.js');
+    fs.writeFileSync(newFileName, restOfTheFile);
 
-    throw "Not Implemented yet"
+    modifiedCLIFile = newFileName;
+
+    // Overwrite logger
+    // We are interested on what it outputs, so we enqueue its messages
+    var myStdOutMessages = new Array();
+    var myLogger = {
+      warn  : function (message) { myStdOutMessages.push('logger-warn: ' + message); }
+    , info  : function (message) { myStdOutMessages.push('logger-info: ' + message); }
+    , write : function (message) { myStdOutMessages.push('logger-write: ' + message); }
+    }
+
+    // We need this custom object to capture the calls to `process`
+    // inside the module
+    var myProcessObj = {
+      argv                  : process.argv,
+      emit                  : process.emit,
+      env                   : {
+        ROOT_PATH           : process.env.ROOT_PATH
+      },
+      exit                  : myProcessObjExit,
+      on                    : process.on
+    }
+
+    function myProcessObjExit (code) {
+      myStdOutMessages.push('-- process.exit() have been called with the code: ' + code);
+    }
+
+    // The last step is overwrite `common` function `present()`
+    var myCommon = require(join('..','lib','agent','common'));
+    myCommon.config.present = function () { return false; }
+    myCommon.logger = myLogger;
+
+    // Create the cli object with dependency injections
+    var cli = sandbox.require(modifiedCLIFile, {
+      requires              : {
+        '/package'          : require(join('..','package.json')),
+        'commander'         : require('commander'),
+        '../utils/pidfile'  : require('../lib/utils/pidfile'),
+        './'                : {
+          run               : function fakeRun () { console.log('Fake run!'); },
+          shutdown          : function fakeShutDown () { console.log('Fake shutdown!'); }
+        },
+        './common'          : myCommon
+      },
+      globals               : {
+        process             : myProcessObj
+      }
+    });
+
+    // The test itself
+    // Note that `myStdOutMessages` depends on what do
+    // we choose to outpu.
+    myStdOutMessages.should.have.length(2)
+    myStdOutMessages[0].should.eql('logger-write: '
+      + '\nNo config file found. Please run bin/prey config.\n');
+    myStdOutMessages[1].should.eql(
+      '-- process.exit() have been called with the code: 1' );
+
+    // Delete the file at the end of this test
+    fs.unlinkSync(modifiedCLIFile);
+
+    done();
   });
 }
 
@@ -192,78 +267,3 @@ it('should store the PID in the pidfile', function(){
 it('should run the main process', function(){
 
 **/
-
-
-/**
- // Useful code I did, but I'm not using right now
-
-
-function generateTestCLI () {
-  before(function () {
-    // Let's create a `cli` file for testing purposes
-    // We must extract the first line, since it has a shebang `#!`
-    var cliPath = join(__dirname, '..', 'lib', 'agent', 'cli.js');
-    var buffer = fs.readFileSync(cliPath);
-    var newline = ['\r'.charCodeAt(0), '\n'.charCodeAt(0)];
-    var i = 0;
-    while (newline.indexOf(buffer[i]) === -1) { i++; }
-    var restOfTheFile = buffer.slice(i + 1, buffer.length).toString('utf-8');
-
-    var randomNumber = Math.floor(1000000000 * Math.random()).toString()
-    var newFileName = join('/tmp', randomNumber + '_lib_agent_cli.js');
-    fs.writeFileSync(newFileName, restOfTheFile);
-
-    modifiedCLIFile = newFileName;
-  })
-
-  it('should set config file path relative to it', function (done) {
-    // We need to modify `process.argv`. If not, commander
-    // will load the parameters of mocha!
-    process.argv = mochaProcessArgv.splice(0,2);
-
-    // We need this custom object to capture the calls to `process`
-    // inside the module
-    var myProcessObj = {
-      argv                  : process.argv,
-      emit                  : process.emit,
-      env                   : {
-        ROOT_PATH           : process.env.ROOT_PATH
-      },
-      exit                  : myProcessObjExit,
-      on                    : process.on
-    }
-
-    // TODO: Find the right way to clone `process`
-
-    function myProcessObjExit (code) {
-      console.log('-- process.exit() have been called with the code: ' + code);
-    }
-
-    // Create the cli object with dependency injections
-    var cli = sandbox.require(modifiedCLIFile, {
-      requires              : {
-        '/package'          : require(join('..','package.json')),
-        'commander'         : require('commander'),
-        '../utils/pidfile'  : require('../lib/utils/pidfile'),
-        './'                : {
-          run               : function fakeRun () { console.log('Fake run!'); },
-          shutdown          : function fakeShutDown () { console.log('Fake shutdown!'); }
-        },
-        './common'          : require(join('..','lib','agent','common'))
-      },
-      globals               : {
-        process             : myProcessObj
-      }
-    });
-
-
-
-  });
-
-  after(function () {
-    fs.unlinkSync(modifiedCLIFile);
-  });
-}
-
-**/
-
