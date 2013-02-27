e134cb181fc8dfd9799ef7543ec4a8d45cac1a20
Herman Junge <herman@forkhq.com>
2013-02-27 13:40:40 -0300

    Heavy refactoring

--- test/00_osx_instalation.js
+++ test/00_osx_instalation.js
@@ -19,156 +19,110 @@ var assert    = require('assert')
 /**
  * Main Suite
  */
-describe('# __HERMAN__ (OSX) Installation', function (){
-  describe('## scripts/create_user.sh#create_user()',
-    testsOSXScriptCreateUserFuncionCreateUser);
-  describe('## scripts/create_user.sh#grant_privileges()',
-    testsOSXScriptCreateUserFuncionGrantPrivileges);
+describe('# __HERMAN__ (OSX) Installation', function () {
+  describe('## scripts/create_user.sh', suiteScriptsCreateUser);
 });
 
-function testsOSXScriptCreateUserFuncionCreateUser () {
+function suiteScriptsCreateUser () {
   // Suite Variables
-  var testDir
-    , dstFile
+  var testDir   = 'prey_exec_test'
+    , dstFile   = path.resolve('/tmp', testDir, 'create_user.sh')
+    , username  = 'test___prey';
 
   before(function (done) {
-    // Prepare test directory
-    testDir = 'prey_exec_test';
-    dstFile = path.resolve('/tmp', testDir, 'create_user.sh');
-    testUtils.generateTestDirectory(testDir, createdDir);
-
-    function createdDir (err) {
-      if (err) throw err;
-      // modify and copy adapted file
-      testUtils.modifyScriptCreateUser('create_user', modifiedFile);
+    var objVars = {
+      testDir     : testDir
+    , dstFile     : dstFile
+    , username    : username
     }
-
-    function modifiedFile (err, data) {
-      if (err) throw err;
-      if (!data) throw new Error ('Bad data!');
-      fs.writeFile(dstFile, data, wroteFile);
-    }
-
-    function wroteFile (err) {
-      if (err) throw err;
-      fs.chmod(dstFile, '777', chmoedFile);
-    }
-
-    function chmoedFile (err) {
-      if (err) throw err;
-      done();
-    }
+    testUtils.prepareTestEnvScriptCreateUser(objVars, done);
   });
 
-  it('Should exit when no username is given', function (done) {
-    var execPath    = dstFile;
-    var execCommand = execPath;
-    testUtils.executeCommand(execCommand, executedCreationCommand);
+  describe('###create_user()', function () {
+    // ### create_user() suite variables
+    var execPath = dstFile;
 
-    function executedCreationCommand (err, response) {
-      err.should.be.equal('User name required.\n');
-      done();
-    }
-  });
+    it('Should exit when no username is given', function (done) {
+      var execCommand = execPath;
+      testUtils.executeCommand(execCommand, executedCreationCommand);
 
-  it('Should create a user, given the username', function (done) {
-    this.timeout(4000);
-    var execPath    = dstFile;
-    var execCommand = execPath + ' test___prey';
-    testUtils.executeCommand(execCommand, executedCreationCommand);
-
-    function executedCreationCommand (err, response) {
-      if (err) throw err;
-      // Let's test if the user was created
-      var execCommand = 'dscl . -read /Users/test___prey';
-      testUtils.executeCommand(execCommand, executedQueryCommand);
-    }
-
-    function executedQueryCommand (err, response) {
-      // If the user was not created, we will have an error
-      if (err) throw err;
-      // Let's check the rest of the values:
-      var userData = response.split('\n');
-      assert(userData.indexOf('UserShell: /bin/bash') !== -1, 'UserShell should be /bin/bash');
-      var indexRealName = userData.indexOf('RealName:');
-      userData[indexRealName + 1].should.be.equal(' Prey Anti-Theft');
-      assert(userData.indexOf('PrimaryGroupID: 80') !== -1, 'PrimaryGroupID should be 80');
-      assert(userData.indexOf('Password: *') !== -1, 'Password should be *');
-
-      // No errors? Cool, let's finish this
-      done();
-    }
-  });
-
-  it('Should exit if it is executed with a user different than root', function (done) {
-    var execPath          = dstFile
-      , execCommand       = 'dscl . -read /Users/test___prey | grep UniqueID'
-      , creationResponse  = ''
-      , expectedText      = '/tmp/' + testDir + '/create_user.sh must be run as root.\n';
-
-    testUtils.executeCommand(execCommand, executedQueryCommand);
-
-    function executedQueryCommand (err, response) {
-      if (err) throw err;
-      var id = parseInt(response.split(' ')[1].replace('\n', ''));
-
-      testUtils.spawnCommand( execPath
-                            , ['test___prey']
-                            , {uid : id}
-                            , executedCreationCommand);
-    }
-
-    function executedCreationCommand (stderr, stdout, exit) {
-      if (stdout) {
-        creationResponse += stdout;
-      }
-      if (exit) {
-        creationResponse.should.be.equal(expectedText);
+      function executedCreationCommand (err, response) {
+        err.should.be.equal('User name required.\n');
         done();
       }
-    }
+    });
+
+    it('Should create a user, given the username', function (done) {
+      this.timeout(4000);
+      var execCommand = execPath + ' ' + username;
+      testUtils.executeCommand(execCommand, executedCreationCommand);
+
+      function executedCreationCommand (err, response) {
+        if (err) throw err;
+        // Let's test if the user was created
+        execCommand = 'dscl . -read /Users/' + username;
+        testUtils.executeCommand(execCommand, executedQueryCommand);
+      }
+
+      function executedQueryCommand (err, response) {
+        // If the user was not created, we will have an error
+        if (err) throw err;
+        // Let's check the rest of the values:
+        var userData = response.split('\n');
+        assert(userData.indexOf('UserShell: /bin/bash') !== -1, 'UserShell should be /bin/bash');
+        var indexRealName = userData.indexOf('RealName:');
+        userData[indexRealName + 1].should.be.equal(' Prey Anti-Theft');
+        assert(userData.indexOf('PrimaryGroupID: 80') !== -1, 'PrimaryGroupID should be 80');
+        assert(userData.indexOf('Password: *') !== -1, 'Password should be *');
+        done();
+      }
+    });
+
+    it('Should exit if it is executed with a user different than root', function (done) {
+      var execCommand       = 'dscl . -read /Users/' + username + ' | grep UniqueID'
+        , creationResponse  = ''
+        , expectedText      = '/tmp/' + testDir + '/create_user.sh must be run as root.\n';
+
+      testUtils.executeCommand(execCommand, executedQueryCommand);
+
+      function executedQueryCommand (err, response) {
+        if (err) throw err;
+        var id = parseInt(response.split(' ')[1].replace('\n', ''));
+        testUtils.spawnCommand( execPath
+                              , [username]
+                              , {uid : id}
+                              , executedCreationCommand);
+      }
+
+      function executedCreationCommand (stderr, stdout, exit) {
+        if (stdout) {
+          creationResponse += stdout;
+        }
+        if (exit) {
+          creationResponse.should.be.equal(expectedText);
+          done();
+        }
+      }
+    });
+
+    it('Should exit if user already exists', function (done) {
+      var execCommand = execPath + ' ' + username;
+      testUtils.executeCommand(execCommand, executedCreationCommand);
+
+      function executedCreationCommand (err, response) {
+        response.should.be.equal(username + ' user already exists!\n'),
+        done();
+      }
+    });
   });
 
-  it('Should exit if user already exists', function (done) {
-    var execPath    = dstFile;
-    var execCommand = execPath + ' test___prey';
-    testUtils.executeCommand(execCommand, executedCreationCommand);
-
-    function executedCreationCommand (err, response) {
-      response.should.be.equal('test___prey user already exists!\n'),
-      done();
-    }
+  describe('###grant_privileges()', function () {
+    it('Should have a Test here', function (done) {
+      throw "Not implemented Yet";
+    });
   });
 
   after(function (done) {
-    // Delete test user (test___prey)
-    var execCommand = 'dscl . -delete /Users/test___prey'
-    testUtils.executeCommand(execCommand, executedDeleteUser);
-
-    function executedDeleteUser (err) {
-      if (err) throw err;
-      // Delete working test directory
-      var command = 'rm -rf /tmp/' + testDir
-      testUtils.executeCommand(command, executedDeleteDir);
-    }
-
-    function executedDeleteDir (err) {
-      if (err) throw err;
-      done();
-    }
+    testUtils.cleanUpScriptCreateUser(testDir, username, done);
   });
-}
-
-function testsOSXScriptCreateUserFuncionGrantPrivileges () {
-  before(function (done) {
-    done();
-  });
-
-  it('Should have a Test here', function (done){
-    throw "Not Implemented Yet";
-  });
-
-  after(function (done) {
-    done();
-  })
 }
--- test/lib/test_utils.js
+++ test/lib/test_utils.js
@@ -32,6 +32,126 @@ utils.getMochaArgv = function (process) {
 }
 
 /**
+ * @param   {Object}    objVars
+ * @param   {String}    username
+ * @param   {Callback}  callback
+ *
+ * @summary Prepare environment for `create_user` tests
+ */
+utils.prepareTestEnvScriptCreateUser = function (objVars, callback) {
+  // Does the test directory exists?
+  fs.stat('/tmp/' + objVars.testDir, foundDir);
+
+  function foundDir (err, data) {
+    if (data) {
+      // delete the dir
+      var command = 'rm -rf /tmp/' + objVars.testDir;
+      return utils.executeCommand(command, executedDeleteDir);
+    }
+    return queryForExistingUser();
+  }
+
+  function executedDeleteDir (err) {
+    if (err) throw err;
+    queryForExistingUser();
+  }
+
+  function queryForExistingUser () {
+    // Does the user `username` exists
+    var execCommand = 'dscl . -read /Users/' + objVars.username;
+    utils.executeCommand(execCommand, executedQueryCommand);
+  }
+
+  function executedQueryCommand (err, response) {
+    if (response) {
+      var execCommand = 'dscl . -delete /Users/' + objVars.username;
+      return utils.executeCommand(execCommand, executedDeleteUser);
+    }
+    return utils.checkForSudoPrivileges(objVars.username, privsChecked);
+  }
+
+  function executedDeleteUser (err) {
+    if (err) throw err;
+    utils.checkForSudoPrivileges(objVars.username, privsChecked);
+  }
+
+  function privsChecked (err) {
+    if (err) throw err;
+    // We are good to go, Prepare test directory
+    return utils.generateTestDirectory(objVars.testDir, createdDir);
+  }
+
+  function createdDir (err) {
+    if (err) throw err;
+    // modify and copy adapted file
+    utils.modifyScriptCreateUser(modifiedFile);
+  }
+
+  function modifiedFile (err, data) {
+    if (err) throw err;
+    if (!data) throw new Error ('Bad data!');
+    fs.writeFile(objVars.dstFile, data, wroteFile);
+  }
+
+  function wroteFile (err) {
+    if (err) throw err;
+    fs.chmod(objVars.dstFile, '777', chmoedFile);
+  }
+
+  function chmoedFile (err) {
+    if (err) throw err;
+    callback();
+  }
+}
+
+/**
+ * @param   {String}    username
+ * @param   {Callback}  callback
+ *
+ * @summary Check for privilege file existence.
+ *          If we find the file, we delete it.
+ */
+utils.checkForSudoPrivileges = function (username, callback) {
+  fs.stat('/etc/sudoers.d/50_prey_switcher', foundFile);
+
+  function foundFile (err, data) {
+    if (data) {
+      fs.unlink('/etc/sudoers.d/50_prey_switcher', deletedFile);
+    }
+  }
+
+  function deletedFile (err) {
+    if (err) throw err;
+    return callback();
+  }
+}
+
+/**
+ * @param   {String}    testDir
+ * @param   {String}    username
+ * @param   {Callback}  callback
+ *
+ * @summary Cleans up directory and user for `create_user` tests
+ */
+utils.cleanUpScriptCreateUser = function (testDir, username, callback) {
+  // Delete test user (test___prey)
+  var execCommand = 'dscl . -delete /Users/' + username;
+  utils.executeCommand(execCommand, executedDeleteUser);
+
+  function executedDeleteUser (err) {
+    if (err) throw err;
+    // Delete working test directory
+    var command = 'rm -rf /tmp/' + testDir
+    utils.executeCommand(command, executedDeleteDir);
+  }
+
+  function executedDeleteDir (err) {
+    if (err) throw err;
+    callback();
+  }
+}
+
+/**
  * @param   {String}    testDir
  * @param   {Callback}  callback
  *
@@ -99,30 +219,21 @@ utils.spawnCommand = function (command, args, options, callback) {
 }
 
 /**
- * @param   {String}    testingFunction
  * @param   {Callback}  callback
  *
  * @summary Copies the file `scripts/create_user.hs` and modifies it
  *          for testing purposes of one single functionality
  */
-utils.modifyScriptCreateUser = function (testingFunction, callback) {
+utils.modifyScriptCreateUser = function (callback) {
   var filePath = path.resolve(__dirname, '../../scripts/create_user.sh')
-    , functionalities = [ 'create_user'
-                        , 'grant_privileges'
-                        , 'test_impersonation' ]
     , index;
 
   fs.readFile(filePath, 'utf8', onFileRead);
 
   function onFileRead (err, data) {
     if (err) return callback(err);
-    // Start the mods: Comment all function calls except the testee
-    functionalities.splice(functionalities.indexOf(testingFunction), 1);
-    functionalities.forEach(function (f) {
-      index = data.match(f + '\n')['index'];
-      data = data.slice(0, index) + '# ' + data.slice(index);
-    });
-
+    index = data.match('test_impersonation\n')['index'];
+    data  = data.slice(0, index) + '# ' + data.slice(index);
     return callback(null, data);
   }
 }
