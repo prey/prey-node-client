fa4996fcafc008908ccda443fd03070e5f4c4524
Herman Junge <herman@forkhq.com>
2013-05-15 15:08:14 -0400

    removing the `it` inside the `it`'s...

--- test/bin_network_trigger_spec.js
+++ test/bin_network_trigger_spec.js
@@ -1,11 +1,11 @@
 
 describe('when called with no arguments', function(){
-  it('it sets prey_bin_path to `/usr/lib/prey/current`');
+  it('sets prey_bin_path to `/usr/lib/prey/current`');
 });
 
 describe('when called with argument', function(){
   describe('and that path does not exist', function(){
-    it('it exits with error code');
+    it('exits with error code');
   });
   describe('and that path exists', function(){
     it('sets prey_bin_path as that one');
@@ -13,24 +13,24 @@ describe('when called with argument', function(){
 });
 
 describe('when a network change is detected', function(){
-  it('it checks if there is internet connection')
+  it('checks if there is internet connection')
   describe('and there is no internet connection', function(){
-    it('it does not call the prey script');
-    it('it keeps running');
+    it('does not call the prey script');
+    it('keeps running');
   });
   describe('and there is internet connection', function(){
     describe('and prey_bin hasn\'t been called', function(){
-      it('it calls the script');
-      it('it keeps running');
+      it('calls the script');
+      it('keeps running');
     });
     describe('and prey_bin has been called', function(){
       describe('and last call time was less than two minutes ago', function(){
-        it('it does not call the script');
-        it('it keeps running');
+        it('does not call the script');
+        it('keeps running');
       });
       describe('and last call time was more than two minutes ago', function(){
-        it('it calls the script');
-        it('it keeps running');
+        it('calls the script');
+        it('keeps running');
       });
     });
   });
