87f2851491480d6a25372dcd94e66eb94ff6a9b0
Herman Junge <herman@forkhq.com>
2013-05-24 12:40:51 -0400

    Start work in prey_conf_spec tests

---
+++ test/prey_conf_spec.js
@@ -0,0 +1,10 @@
+
+
+describe('prey_conf_spec #wip', function(){
+  it('it should be valid ini format (with # instead of ; though)');
+  it('driver should be set to control-panel');
+  it('host should be set to control.preyproject.com');
+  it('protocol should be set to https');
+  it('api_key should be empty');
+  it('device_key should be empty');
+});
