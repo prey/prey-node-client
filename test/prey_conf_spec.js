d7a0ac9daeadff14534e53907cf09f0664692eaf
Herman Junge <herman@forkhq.com>
2013-05-28 13:21:51 -0300

    Corrections for windows (\r\n)

--- test/prey_conf_spec.js
+++ test/prey_conf_spec.js
@@ -1,13 +1,14 @@
 
-var fs      = require('fs'),
-    join    = require('path').join,
-    should  = require('should');
+var fs                  = require('fs'),
+    join                = require('path').join,
+    should              = require('should'),
+    is_windows          = process.platform === 'win32',
+    conf_file_contents  = fs.readFileSync(join(__dirname, '..', 'prey.conf.default'), 'utf8');
 
 describe('prey_conf_spec', function(){
-  var conf_file_path = join(__dirname, '..', 'prey.conf.default');
 
   it('it should be valid ini format (with # instead of ; though)', function(){
-    var lines = fs.readFileSync(conf_file_path, 'utf8').split('\n');
+    var lines = conf_file_contents.split('\n');
 
     lines.forEach(function(line){
       if (line.length > 0)
@@ -16,27 +17,37 @@ describe('prey_conf_spec', function(){
   });
 
   it('driver should be set to control-panel', function(){
-    var driver = fs.readFileSync(conf_file_path, 'utf8').match(/\ndriver\s=\s(.*)\n/)[1]
+    var driver = is_windows?
+      conf_file_contents.match(/\r\ndriver\s=\s(.*)\r\n/)[1] :
+      conf_file_contents.match(/\ndriver\s=\s(.*)\n/)[1];
     driver.should.be.equal('control-panel');
   });
 
   it('host should be set to control.preyproject.com', function(){
-    var host = fs.readFileSync(conf_file_path, 'utf8').match(/\nhost\s=\s(.*)\n/)[1]
+    var host = is_windows?
+      conf_file_contents.match(/\r\nhost\s=\s(.*)\r\n/)[1] :
+      conf_file_contents.match(/\nhost\s=\s(.*)\n/)[1];
     host.should.be.equal('control.preyproject.com');
   });
 
   it('protocol should be set to https', function(){
-    var protocol = fs.readFileSync(conf_file_path, 'utf8').match(/\nprotocol\s=\s(.*)\n/)[1]
+    var protocol = is_windows?
+      conf_file_contents.match(/\r\nprotocol\s=\s(.*)\r\n/)[1] :
+      conf_file_contents.match(/\nprotocol\s=\s(.*)\n/)[1];
     protocol.should.be.equal('https');
   });
 
   it('api_key should be empty', function(){
-    var api_key = fs.readFileSync(conf_file_path, 'utf8').match(/\napi_key\s=\s(.*)\n/)[1]
+    var api_key = is_windows?
+      conf_file_contents.match(/\r\napi_key\s=\s(.*)\r\n/)[1] :
+      conf_file_contents.match(/\napi_key\s=\s(.*)\n/)[1];
     api_key.should.be.empty;
   });
 
   it('device_key should be empty', function(){
-    var device_key = fs.readFileSync(conf_file_path, 'utf8').match(/\ndevice_key\s=\s(.*)\n/)[1]
+    var device_key = is_windows?
+      conf_file_contents.match(/\r\ndevice_key\s=\s(.*)\r\n/)[1] :
+      conf_file_contents.match(/\ndevice_key\s=\s(.*)\n/)[1];
     device_key.should.be.empty;
   });
 });
