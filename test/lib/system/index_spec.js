31c728d7926cd1c0f7079a44f5e454fa5c7f3206
Herman Junge <herman@forkhq.com>
2013-06-03 16:12:18 -0400

    WIP `spawn_as_logged_user()` in `lib/system/index_spec.js`

--- test/lib/system/index_spec.js
+++ test/lib/system/index_spec.js
@@ -1,10 +1,10 @@
 
 var join        = require('path').join,
     should      = require('should'),
-    index_path  = join(__dirname, '..', '..', '..', 'lib', 'system', 'index.js'); 
+    index_path  = join(__dirname, '..', '..', '..', 'lib', 'system', 'index.js'),
+    is_windows  = process.platform === 'win32';
 
 describe('lib/system/index_spec.js #wip', function(){
-
   var index = require(index_path);
 
   describe('get_logged_user()', function(){
@@ -27,15 +27,33 @@ describe('lib/system/index_spec.js #wip', function(){
     });
   });
 
-  describe('spawn_as_logged_user()', function(){
-    it('----');
+  describe('spawn_as_logged_user() #wip2', function(){
+
+    if (is_windows) {
+      it('should do windows stuff ---- I need a command here');
+    } else {
+      it('should issue a `whoami` using the function', function(done){
+        index.spawn_as_logged_user('whoami', [], function(err, spawned_item){
+          var response;
+          spawned_item.stdout.on('data', function (data){ response = data.toString('utf8');});
+          spawned_item.on('close', function (code){
+            response.length.should.be.above(0);
+            code.should.be.equal(0);
+            done();
+          });
+        });
+      });
+    }
   });
   describe('run_as_logged_user()', function(){
+
     it('----');
   });
+
   describe('get_running_user()', function(){
     it('----');
   });
+
   describe('get_os_info() -> get_os_version + get_os_name', function(){
     it('----');
   });
