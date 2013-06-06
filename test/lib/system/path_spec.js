f855deedf0d0fc0332039c0e29fe14d063ddfbbb
Herman Junge <herman@forkhq.com>
2013-06-06 12:07:24 -0400

    Added scaffold for `lib/system/path_spec`

---
+++ test/lib/system/path_spec.js
@@ -0,0 +1,32 @@
+
+describe('lib/system/path_spec #wip', function(){
+
+  describe('when installed under a versions folder', function(){
+    describe('and current folder is a symlink', function(){
+      it('path.package should be equal to ``');
+      it('path.install should be equal to ``');
+      it('path.current should be equal to ``');
+      it('path.versions should be equal to ``');
+      it('path.package_bin should be equal to ``');
+      it('path.current_bin should be equal to ``');
+    });
+
+    describe('and current folder is not a symlink (WinXP)', function(){
+      it('path.package should be equal to ``');
+      it('path.install should be equal to ``');
+      it('path.current should be equal to ``');
+      it('path.versions should be equal to ``');
+      it('path.package_bin should be equal to ``');
+      it('path.current_bin should be equal to ``');
+    });
+  });
+
+  describe('when NOT installed under a versions folder (eg. via npm)', function(){
+    it('path.package should be equal to ``');
+    it('path.install should be equal to ``');
+    it('path.current should be equal to ``');
+    it('path.versions should be equal to ``');
+    it('path.package_bin should be equal to ``');
+    it('path.current_bin should be equal to ``');
+  });
+});
