94f9f4c357b8cc92e959070458a46824b32bb4c8
Herman Junge <herman@forkhq.com>
2013-06-12 17:13:54 -0400

    Add two tests to suite `lib/agent/index`

--- test/lib/agent/index.js
+++ test/lib/agent/index.js
@@ -1,5 +1,6 @@
 
-var join              = require('path').join,
+var fs                = require('fs'),
+    join              = require('path').join,
     sandbox           = require('sandboxed-module'),
     utils             = require(join(__dirname,'..', '..', 'utils', 'test_utils.js')),
     agent_index_path  = join(__dirname, '..', '..', '..', 'lib', 'agent', 'index.js'),
@@ -82,10 +83,17 @@ describe('lib/agent/index #wip', function(){
               hooks_called.push(hook);
             }
           }
+          var loader  = {
+            load_driver : function(){
+              var _cb = arguments[1];
+              return _cb(new Error('End Test'));
+            }
+          }
           var requires = {
             './common'    : common,
             './updater'   : updater,
-            './hooks'     : hooks
+            './hooks'     : hooks,
+            './loader'    : loader
           };
           index = sandbox.require(agent_index_path, { requires : requires });
         });
@@ -100,5 +108,99 @@ describe('lib/agent/index #wip', function(){
         });
       })
     });
+  });
+
+  describe('engage()', function(){
+
+    var index, hooks_triggered;
+
+    before(function(){
+      var common = utils.create_common_object();
+      hooks_triggered = [];
+      var hooks = {
+        trigger : function(hook, type) {
+          var obj = {};
+          obj[hook] = type;
+          hooks_triggered.push(obj);
+        }
+      }
+      var requires = {
+        './common'    : common,
+        './hooks'     : hooks
+      };
+      index = sandbox.require(agent_index_path, { requires : requires });
+    });
+
+    it('should trigger `woken` hook with the param given', function(done){
+      index.engage('engage_type');
+      hooks_triggered[0].should.have.property('woken');
+      hooks_triggered[0]['woken'] = 'engage_type';
+      done();
+    });
+  });
+
+  describe('shutdown() #wip2', function(){
+
+    describe('when there are loaded drivers', function(){
+
+      var index, spy_unload_called = {};
+
+      before(function(){
+        var common = utils.create_common_object();
+        common.program.run = false;
+        var hooks   = {
+          unload : function() { return; }
+        }
+        var updater = {
+          check : function(cb){ return cb(null, false); }
+        }
+        var requires = {
+          './common'    : common,
+          './hooks'     : hooks,
+          './updater'   : updater,
+        };
+
+        // We need drivers exposed in here...
+        var agent_index_contents = require('fs').readFileSync(agent_index_path, 'utf8');
+        agent_index_contents += '\nexports.drivers = drivers;'
+        fs.renameSync(agent_index_path, agent_index_path + '.tmp');
+        fs.writeFileSync(agent_index_path, agent_index_contents, { mode: 0755 });
+
+        index = sandbox.require(agent_index_path, { requires : requires });
+
+        // Then we force this up (remember `drivers` is private)
+        index.drivers['test_driver_alpha'] = {
+          unload : function() {
+            spy_unload_called['alpha'] = true;
+          }
+        }
+        index.drivers['test_driver_beta'] = {
+          unload : function() {
+            spy_unload_called['beta'] = true;
+          }
+        }
+      });
+
+      it('should unload drivers', function(done){
+        index.shutdown();
+        spy_unload_called.should.have.property('alpha');
+        spy_unload_called.should.have.property('beta');
+        done();
+      });
+
+      after(function(done){
+        fs.unlinkSync(agent_index_path);
+        fs.rename(agent_index_path + '.tmp', agent_index_path, done)
+      });
+    });
+
+    describe('when there are loaded hooks', function(){
+      it('should unload hooks');
+    });
+    
+    it('should cancel reports');
+    it('should unwatch triggers');
+    it('should clean up files');
+    it('should set `running` as false');
   });
 });
