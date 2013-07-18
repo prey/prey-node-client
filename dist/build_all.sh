5043c201313301202cca70319d0685e142c569ee
Tomás Pollak <tomaspollak@gmail.com>
2013-07-18 11:43:15 -0400

    Checks in build_all.sh.

--- dist/build_all.sh
+++ dist/build_all.sh
@@ -5,22 +5,23 @@ version=$1
 [ -z "$1" ] && echo "Version required." && exit 1
 [ ! -d "./${version}" ] && echo "Directory not found!" && exit 1
 
-echo "Building OSX packages..."
-cd mac
-./build.sh $version
-cd ..
+if [ -d "mac" ]; then
+  echo "Building OSX packages..."
+  cd mac
+  ./build.sh $version
+  cd ..
+fi
 
-echo "Building Linux packages..."
-cd linux
-./build.sh $version
-cd ..
+if [ -d "linux" ]; then
+  echo "Building Linux packages..."
+  cd linux
+  ./build.sh $version
+  cd ..
+fi
 
-#
-# Commenting Windows package making. We need to run
-# a portion of the process in a windows machine.
-#
-# echo "Building Windows packages..."
-# cd windows
-# ./build.sh $version
-# cd ..
-
+if [ -d "windows" ]; then
+  echo "Building Windows packages..."
+  cd windows
+ ./build.sh $version
+  cd ..
+fi
