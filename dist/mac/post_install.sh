e672033a6ac08afdf38d75e0664b762b0c3b5108
Tomás Pollak <tomaspollak@gmail.com>
2013-07-19 19:30:56 -0400

    Updated Linux/OSX postinstall scripts.

--- dist/mac/post_install.sh
+++ dist/mac/post_install.sh
@@ -6,18 +6,19 @@ BASE_PATH="/usr/lib/prey"
 CONFIG_DIR="/etc/prey"
 LOG_FILE="/var/log/prey.log"
 INSTALL_PATH="${BASE_PATH}/versions/${VERSION}"
-BIN_PATH="${INSTALL_PATH}/bin/prey"
+PREY_BIN="bin/prey"
 
-bash "$INSTALL_PATH/scripts/create_user.sh" ${PREY_USER}
+bash "$INSTALL_PATH/scripts/create_user.sh" ${PREY_USER} || true
 mkdir -p $CONFIG_DIR
 touch $LOG_FILE
 
 # set up permissions
 chown -R $PREY_USER: $CONFIG_DIR $BASE_PATH $LOG_FILE
-# as prey_user: symlink /current, write crontab, generate prey.conf
-su ${PREY_USER} -c "$BIN_PATH config activate"
 
+cd "$INSTALL_PATH"
+# as prey_user: symlink /current, generate prey.conf
+su ${PREY_USER} -c "$PREY_BIN config activate"
 # as root user: setup system/init scripts and fire up gui
 # (root required for guest account creation)
-"$BIN_PATH" config hooks post_install
-"$BIN_PATH" config gui
+"$PREY_BIN" config hooks post_install
+"$PREY_BIN" config gui
