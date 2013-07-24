412e034004a0e6ca3da200cd7c5e7cf678d0b517
Herman Junge <herman@forkhq.com>
2013-07-24 19:45:44 -0400

    Test calls `bin/prey config upgrade`

--- test/lib/agent/updating_spec.js
+++ test/lib/agent/updating_spec.js
@@ -1,22 +1,25 @@
 
-var join            = require('path').join,
-    sandbox         = require('sandboxed-module'),
-    common_path     = join(__dirname, '..', '..', '..', 'lib', 'agent', 'common'),
-    package_path    = join(__dirname, '..', '..', '..', 'lib', 'conf', 'package'),
-    updater_path    = join(__dirname, '..', '..', '..', 'lib', 'agent', 'updater');
+var join                = require('path').join,
+    sandbox             = require('sandboxed-module'),
+    common_path         = join(__dirname, '..', '..', '..', 'lib', 'agent', 'common'),
+    package_path        = join(__dirname, '..', '..', '..', 'lib', 'conf', 'package'),
+    package_json_path   = join(__dirname, '..', '..', '..', 'package.json')
+    updater_path        = join(__dirname, '..', '..', '..', 'lib', 'agent', 'updater');
 
 describe('updating', function(){
 
   describe('when there is versions support', function(){
     // Suite Variables
-    var updater,
+    var common,
+        package,
+        updater,
         flag_package_check_latest_version_called = false;
 
     before(function(){
-      var common            = require(common_path);
+      common                = require(common_path);
       system                = common.system;
-      system.paths.versions = true; // So we can modify `can_upgrade`
-      var package           = {
+      system.paths.versions = 'true'; // So we can modify `can_upgrade`
+      package               = {
         check_latest_version : function () {
           flag_package_check_latest_version_called = true;
           return;
@@ -35,35 +38,35 @@ describe('updating', function(){
 
     describe('when current version is older', function(){
 
-      describe('and has not write permissions', function(){
-
-        it('does not update the package');
+      before(function(){
+        common.helpers.is_greater_than = function () {
+          return true;
+        }
+        package           = {
+          check_latest_version : function (callback) {
+            callback(null, 'x.y.z');
+          }
+        }
+        system                    = common.system;
+        system.paths.package_bin  = join(__dirname, '..', '..', '..', 'test', 'utils', 'fake_bin_prey_empty');
+        var sandbox_options       = { requires : {} };
+        sandbox_options.requires['./common']    = common;
+        sandbox_options.requires[package_path]  = package;
+        updater = sandbox.require(updater_path, sandbox_options);
       });
 
-      describe('and has write permissions', function(){
+      it('calls `bin/prey config upgrade` ', function (){
+        updater.check();
 
-        it('updates the package');
+        // Check the output somehow
 
-        describe('and the config file was modified', function(){
-
-          describe('and has no write permissions', function(){
-
-            it('leaves the file untouched');
-          })
-
-          describe('and has write permissions', function(){
-
-            it('adds new keys if any');
-
-            it('does not replace any existing values');
-
-            it('updates for existing keys');
-          });
-        });
-
-        it('exits the program');
       });
     });
+
+
+
+
+
   });
 
   describe('when there is NOT versions support', function(){
