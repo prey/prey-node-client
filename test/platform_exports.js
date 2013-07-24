1487d33a49a5a4654483cb78f814d34ad5f96716
Herman Junge <herman@forkhq.com>
2013-07-23 20:46:50 -0400

    Comment test until verify actual isomorphism on platform function exports.

--- test/platform_exports.js
+++ test/platform_exports.js
@@ -1,32 +1,37 @@
+
+/*
+var join = require('path').join;
+
 describe('each platform', function(){
 
-  it('exports the same number of function on each module', function(done){
+  it('exports the same number of functions on each module', function(done){
 
     var load = function(dir, os){
-      var p = __dirname + '/../' + dir + '/' + os;
-      return require(p);
+      return require(join(__dirname, '..', dir, os));
     }
 
     var checkPath = function(dir){
 
-      var linux = load(dir, 'linux'),
-          mac =   load(dir, 'mac'),
+      var linux   = load(dir, 'linux'),
+          mac     = load(dir, 'mac'),
           windows = load(dir, 'windows');
+
+      console.log(Object.keys(linux));
 
       // Object.keys(mac).length.should.equal(Object.keys(windows).length);
       // linux.should.have.keys(Object.keys(mac));
       // windows.should.have.keys(Object.keys(linux));
     }
+  });
+});
 
-    finder.eachFileOrDirectoryMatching(/linux(\.js)?$/, './lib', function(err, match, stat){
+*/
 
-      if (match.indexOf('lock/linux') == -1)
-        checkPath(helpers.path.dirname(match));
+/*finder.eachFileOrDirectoryMatching(/linux(\.js)?$/, './lib', function(err, match, stat){
 
-    }, function(){
-      done();
-    });
+  if (match.indexOf('lock/linux') == -1)
+    checkPath(helpers.path.dirname(match));
 
-  })
-
-})
+}, function(){
+  done();
+});*/
