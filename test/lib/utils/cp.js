c54ca97148e96c40ad6f6230868ab1d0f5c123cf
Herman Junge <herman@forkhq.com>
2013-07-29 09:39:10 -0400

    Unfinished tests as pending

--- test/lib/utils/cp.js
+++ test/lib/utils/cp.js
@@ -2,9 +2,7 @@ describe('copy_file', function(){
 
 	describe('if source file does not exist', function(){
 
-		it('should call back with error', function(){
-
-		})
+		it('should call back with error')
 
 	})
 
@@ -12,9 +10,7 @@ describe('copy_file', function(){
 
 		describe('and destination file exists', function(){
 
-			it('should call back with an error', function(){
-
-			})
+			it('should call back with an error')
 
 		})
 
@@ -22,36 +18,23 @@ describe('copy_file', function(){
 
 			describe('and destination folder does NOT exist', function(){
 
-				it('should try to create it', function(){
+				it('should try to create it')
 
-				})
-
-				it('should stop and callback with error if it couldnt', function(){
-
-				})
+				it('should stop and callback with error if it couldnt')
 
 			});
 
 
-			it('should copy file', function(){
+			it('should copy file')
 
-
-			})
-
-			it('should have the same contents', function(){
-
-			})
+			it('should have the same contents')
 
 		})
 
 	})
 
-	it('should callback with error if source file does not exist', function(){
+	it('should callback with error if source file does not exist');
 
-	});
-
-	it('should callback with error if destination folder does not exist', function(){
-
-	});
+	it('should callback with error if destination folder does not exist');
 
 })
--- test/lib/utils/pidfile.js
+++ test/lib/utils/pidfile.js
@@ -1,28 +1,20 @@
 	describe('store_pid', function(){
 
-		it('should verify if pidfile exists', function(){
-
-		});
+		it('should verify if pidfile exists');
 
 		describe('and when pidfile exists', function(){
 
-			it('should verify if the process is running', function(){
-
-			});
+			it('should verify if the process is running');
 
 			describe('and process is running', function(){
 
-				it('should return the processes PID in the callback', function(){
-
-				});
+				it('should return the processes PID in the callback');
 
 			});
 
 			describe("and process is NOT running", function(){
 
-				it("should notify about this and continue", function(){
-
-				});
+				it("should notify about this and continue");
 
 			});
 
@@ -30,13 +22,9 @@
 
 		describe('and when pidfile does NOT exist', function(){
 
-			it('should store the pid in a new file', function(){
+			it('should store the pid in a new file');
 
-			});
-
-			it('should call back with an exception if it had any problems', function(){
-
-			});
+			it('should call back with an exception if it had any problems');
 
 		});
 
