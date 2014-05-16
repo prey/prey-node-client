39ff513acfa2c8f36beb08fbf9df77a2ee3907e9
Tomás Pollak <tomaspollak@gmail.com>
2014-05-15 17:35:36 -0700

    Conf cli tests.

--- test/lib/conf/cli.js
+++ test/lib/conf/cli.js
@@ -1,48 +1,152 @@
-var join   = require('path').join,
-    spawn  = require('child_process').spawn,
-    should = require('should');
+var fs      = require('fs'),
+    join    = require('path').join,
+    spawn   = require('child_process').spawn,
+    should  = require('should'),
+    sandbox = require('./../../utils/spawner_sandbox'),
+    helpers = require('./../../helpers');
 
 var prey_bin = join(__dirname, '..', '..', '..', 'bin', 'prey');
 
+// utility mirror function that returns whatever you pass to it
+var mirror   = function(obj) { return obj };
+
+var common_base = {
+  os_name: 'linux',
+  system: {
+    tempfile_path: mirror,
+    paths: {}
+  },
+  program: {
+    logfile: '/tmp/something.log'
+  }
+}
+
 describe('config cli', function() {
- 
+
+  var called = false;
+  var conf_cli_file = './lib/conf/cli.js';
+
   var run_cli = function(args, cb) {
     var out, err, child = spawn(prey_bin, args);
     child.stdout.on('data', function(data) { out += data });
-    child.stderr.on('data', function(data) { out += data });
+    child.stderr.on('data', function(data) { err += data });
     child.on('exit', function(code) { cb(code, out, err) });
   };
- 
+
+  var unsandboxize_cli = function(done) {
+    sandbox.release(conf_cli_file, done);
+  }
+
   describe('when no arguments are passed', function(){
-    
+
     it('shows help and exits')
-    
+
     it('returns status code 1')
-    
+
   })
-  
+
   describe('activate', function(){
-    
+
   })
-  
-  describe('upgrade', function(){
-    
+
+  describe('upgrade', function() {
+
+    describe('with no arguments', function() {
+
+      describe('with no versions support', function() {
+
+        before(function(done) {
+          var deps = {
+            './../common' : common_base,
+          }
+
+          sandbox.put(conf_cli_file, deps, done);
+        })
+
+        after(unsandboxize_cli)
+
+        it('fails miserably', function(done) {
+
+          run_cli(['config', 'upgrade'], function(code, out, err) {
+            should.not.exist(out);
+            code.should.equal(0); // TODO: this should be 1
+            done();
+          })
+
+        })
+
+      })
+
+      describe('with versions support', function() {
+
+        before(function(done) {
+          var common = common_base;
+          common.system.paths = { versions: '/tmp/versions' };
+
+          var deps = {
+            './../common' : common,
+            './versions'  : { latest: function() { return '0.9.2' } },
+            './../package': { get_latest: function(latest, dest, cb) { return cb(new Error('Called!')) } }
+          }
+
+          sandbox.put(conf_cli_file, deps, done);
+        })
+
+        after(unsandboxize_cli)
+
+        it('tries to fetch latest version', function(done) {
+
+          run_cli(['config', 'upgrade'], function(code, out, err) {
+            out.should.include('Error! Called!');
+            code.should.equal(1);
+            done();
+          })
+
+        })
+
+      })
+
+    })
+
+    describe('with a -v argument', function() {
+
+    })
+
   })
-  
+
   describe('settings', function(){
-    
+
   })
-  
+
   describe('account', function() {
-    
 
     describe('verify', function() {
 
+      before(function(done) {
+        var deps = {
+          './../common' : common_base,
+          './../api': { keys: { verify: function(keys, cb) { return cb(new Error('Called!')) } } }
+        }
+
+        sandbox.put(conf_cli_file, deps, done);
+      })
+
+      after(unsandboxize_cli)
+
+      it('tries to verify keys', function(done){
+
+        run_cli(['config', 'account', 'verify', '-a', 'foobar', '-d', 'barbaz'], function(code, out, err) {
+          out.should.include('Error! Called!');
+          done();
+        })
+
+      })
+
       describe('with invalid api key and email', function() {
 
-        it('returns error code 1', function(done){ 
+        it('returns error code 1', function(done){
 
-          run_cli(['account', 'verify', '-a', 'foobar', '-d', 'barbaz'], function(code, out) {
+          run_cli(['config', 'account', 'verify', '-a', 'invalid', '-d', 'blablabla'], function(code, out, err) {
             code.should.equal(1);
             done();
           })
@@ -56,27 +160,27 @@ describe('config cli', function() {
   })
 
   describe('hooks', function(){
-    
+
     describe('post_install', function(){
-  
+
     })
-    
+
     describe('pre_uninstall', function(){
-      
+
     })
-    
+
   })
 
   describe('check', function(){
-    
+
   })
-  
+
   describe('run', function(){
-    
+
   })
 
   describe('gui', function(){
-    
+
   })
-  
+
 })
---
+++ test/utils/spawner_sandbox.js
@@ -0,0 +1,56 @@
+var fs = require('fs');
+
+exports.put = function(file, deps, cb) {
+
+  var serialize = function(obj) {
+    return JSON.stringify(obj, function(key, val) {
+      if (typeof val === 'function') {
+        // return val.toString();
+        return '____' + val + '____';
+      }
+      return val;
+    });
+  }
+
+  var replace_deps = function(str, deps) {
+    for (var name in deps) {
+      var regex = new RegExp("require.*" + name + ".*\\)");
+      if (str.match(regex)) {
+        str = str.replace(regex, serialize(deps[name]));
+      }
+    }
+    return str.replace(/\"?____\"?/g, '');
+  }
+
+  exports.release(file, function(err) {
+    if (!err) console.log('Unclean previous execution.');
+
+    fs.readFile(file, function(err, data) {
+      if (err) return cb(err);
+
+      var modified = replace_deps(data.toString(), deps);
+      // console.log(modified);
+
+      fs.rename(file, file + '.original', function(err) {
+        if (err) return cb(err);
+
+        fs.writeFile(file, modified, cb);
+      })
+
+    })
+  })
+
+}
+
+exports.release = function(file, cb) {
+  fs.exists(file + '.original', function(exists) {
+    if (!exists)
+      return cb(new Error('Original file not found.'));
+
+    fs.unlink(file, function(err) {
+      if (err) return cb(err);
+
+      fs.rename(file + '.original', file, cb);
+    })
+  })
+}
