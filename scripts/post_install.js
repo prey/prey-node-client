1a684e775e4b704960cf0637ea9219198422de6d
Tomás Pollak <tomaspollak@gmail.com>
2014-07-24 12:33:40 -0700

    Check if bin/prey shebang is OK on scripts/post_install script.

--- scripts/post_install.js
+++ scripts/post_install.js
@@ -3,13 +3,38 @@
 // this script is executed by npm after an 'npm install prey'
 // and calls the conf module to set up config and execution triggers
 
-var join        = require('path').join,
+var fs          = require('fs'),
+    join        = require('path').join,
     execFile    = require('child_process').execFile,
-    prey_bin    = join(__dirname, '..', 'bin', 'prey'),
+    bin_path    = join(__dirname, '..', 'bin'),
+    prey_bin    = join(bin_path, 'prey'),
     args        = ['config', 'hooks', 'post_install'],
     is_windows  = process.platform === 'win32';
 
+if (is_windows) prey_bin += '.cmd';
+
 var line = '\n=====================================================\n';
+
+var check_shebang = function(cb) {
+  if (is_windows) 
+    return cb();
+
+  var node_bin = join(bin_path, 'node');
+  fs.exists(node_bin, function(exists) {
+    if (exists) return cb();
+
+    fs.readFile(prey_bin, function(err, data) {
+      if (err) return cb(err);
+      
+      // replace relative shebang with regular, system-wide one
+      var str = data.toString().replace(/^#! bin\/node/, '#!/usr/bin/env node');
+      if (str == data.toString())
+        return cb();
+
+      fs.writeFile(prey_bin, str, cb);
+    })
+  })
+}
 
 var post_install = function() {
 
@@ -24,8 +49,6 @@ var post_install = function() {
     }
   }
 
-  if (is_windows) prey_bin += '.cmd';
-
   execFile(prey_bin, args, function(err, stdout, stderr) {
     if (stdout.length > 0) console.log(stdout.trim());
     if (stderr.length > 0) console.log(stderr.trim());
@@ -35,5 +58,9 @@ var post_install = function() {
 
 }
 
-if (!process.env.BUNDLE_ONLY)
-  post_install();
+if (!process.env.BUNDLE_ONLY) {
+  check_shebang(function(err) {
+    if (err) throw err;
+    post_install();
+  });
+}
