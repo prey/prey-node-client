15a8ac3a6e1b507de22bd0e0102e245e3aa556c1
Tomás Pollak <tomaspollak@gmail.com>
2014-08-25 12:03:56 -0700

    post_install tests.

--- test/lib/conf/tasks.js
+++ test/lib/conf/tasks.js
@@ -3,6 +3,7 @@ var fs      = require('fs'),
     should  = require('should'),
     sinon   = require('sinon'),
     rimraf  = require('rimraf'),
+    async   = require('async'),
     helpers = require('./../../helpers'),
     tmpdir  = require('os').tmpdir();
 
@@ -10,6 +11,11 @@ var getset  = require('getset');
 var common  = require(helpers.lib_path('common'));
 var tasks   = require(helpers.lib_path('conf', 'tasks'));
 var vm      = require(helpers.lib_path('conf', 'shared', 'version_manager'));
+
+var prey_user = require(helpers.lib_path('conf', 'tasks', 'prey_user'));
+
+var hooks  = require(helpers.lib_path('conf', 'tasks', 'os', process.platform ));
+var daemon = require(helpers.lib_path('conf', 'tasks', 'daemon'));
 
 describe('tasks', function() {
 
@@ -315,8 +321,11 @@ describe('tasks', function() {
               var install_dir = tmpdir + '/install';
 
               before(function(done) {
-                fs.mkdir(version_dir);
-                fs.writeFile(version_dir + '/package.json', JSON.stringify({ version: '2.3.4'}), done);
+                fs.mkdir(version_dir, function(err) {
+                  if (err) return done(err);
+
+                  fs.writeFile(version_dir + '/package.json', JSON.stringify({ version: '2.3.4'}), done);
+                });
               })
 
               after(function(done) {
@@ -388,6 +397,291 @@ describe('tasks', function() {
 
   describe('post_install', function() {
 
+    var spy, stub;
+
+    describe('stage one', function() {
+
+      var sync_stub, old_versions_path;
+
+      before(function() {
+        // let's assume we have a versions path and that config.sync works
+        // we're already testing the versions/no versions logic in .activate()
+        old_versions_path = common.system.paths.versions;
+        common.system.paths.versions = tmpdir + '/versions';
+
+        sync_stub = sinon.stub(common.config, 'sync', function(other_file, method, cb) { cb() } )
+      })
+
+      after(function() {
+        common.system.paths.versions = old_versions_path;
+        sync_stub.restore();
+      })
+
+      describe('on windows', function() {
+
+        var old_platform;
+
+        before(function() {
+          // force platform to win32
+          old_platform = process.platform;
+          process.platform = 'win32';
+        })
+
+        after(function() {
+          // revert platform
+          process.platform = old_platform;
+        })
+
+        describe('if activation returns error', function() {
+
+          describe('and error is "already set as current"', function() {
+
+            before(function() {
+              stub = sinon.stub(vm, 'set_current', function(version, cb) {
+                var err = new Error('Already current');
+                err.code = 'ALREADY_CURRENT';
+                cb(err);
+              })
+            })
+
+            after(function() {
+              stub.restore();
+            })
+
+            it('advances to stage two', function(done) {
+              var async_stub = sinon.stub(async, 'series', function(fx_arr, cb) {
+                return cb(new Error('async.series called.'));
+              })
+
+              tasks.post_install({}, function(err) {
+                err.should.exist;
+                err.message.should.eql('async.series called.');
+                async_stub.restore();
+                done();
+              })
+            })
+
+          })
+
+          describe('and error is something else', function() {
+
+            before(function() {
+              stub = sinon.stub(vm, 'set_current', function(version, cb) {
+                var err = new Error('Foobar.');
+                cb(err);
+              })
+            })
+
+            after(function() {
+              stub.restore();
+            })
+
+            it('does not advance to stage two', function(done) {
+              var async_spy = sinon.spy(async, 'series');
+
+              tasks.post_install({}, function(err) {
+                async_spy.called.should.be.false;
+                async_spy.restore();
+                done();
+              })
+            })
+          })
+
+        })
+
+        describe('if activation succeeds', function() {
+
+          before(function() {
+            stub = sinon.stub(vm, 'set_current', function(version, cb) { cb() });
+          })
+
+          after(function() {
+            stub.restore();
+          })
+
+          it('advances to stage two', function(done) {
+            var async_stub = sinon.stub(async, 'series', function(fx_arr, cb) {
+              return cb(new Error('async.series called.'));
+            })
+
+            tasks.post_install({}, function(err) {
+              async_stub.restore();
+              err.should.exist;
+              err.message.should.eql('async.series called.');
+              done();
+            })
+          })
+
+        })
+
+      })
+
+    })
+
+    describe('non windows', function() {
+
+      var old_platform;
+
+      before(function() {
+        // force platform to darwin
+        old_platform = process.platform;
+        process.platform = 'darwin';
+      })
+
+      after(function() {
+        // revert platform
+        process.platform = old_platform;
+      })
+
+      describe('if prey_user.create fails', function() {
+
+        before(function() {
+          stub = sinon.stub(prey_user, 'create', function(cb) { cb(new Error('Gave up')) });
+        })
+
+        after(function() {
+          stub.restore();
+        })
+
+        it('does not advance to stage two', function(done) {
+          var async_spy = sinon.spy(async, 'series');
+
+          tasks.post_install({}, function(err) {
+            async_spy.called.should.be.false;
+            async_spy.restore();
+            done();
+          })
+        })
+
+      })
+
+      describe('if prey_user.create succeeds', function() {
+
+        before(function() {
+          stub = sinon.stub(prey_user, 'create', function(cb) { cb() });
+        })
+
+        after(function() {
+          stub.restore();
+        })
+
+        it('advances to stage two', function(done) {
+          var async_stub = sinon.stub(async, 'series', function(fx_arr, cb) {
+            return cb(new Error('async.series called.'));
+          })
+
+          tasks.post_install({}, function(err) {
+            err.should.exist;
+            err.message.should.eql('async.series called.');
+            async_stub.restore();
+            done();
+          })
+        })
+
+      });
+
+    })
+
+    describe('stage two', function() {
+
+      var create_user_stub;
+
+      before(function() {
+        // start by assuming that create user was ok
+        create_user_stub = sinon.stub(prey_user, 'create', function(cb) { cb() });
+      })
+
+      after(function() {
+        create_user_stub.restore();
+      })
+
+      describe('if daemon.install fails', function() {
+
+        before(function() {
+          stub = sinon.stub(daemon, 'install', function(cb) { cb(new Error('No daemons around.')) })
+        })
+
+        after(function() {
+          stub.restore();
+        })
+
+        it('returns error', function(done) {
+          tasks.post_install({}, function(err) {
+            err.should.exist;
+            err.message.should.eql('No daemons around.');
+            done();
+          })
+        })
+
+        it('does not run post_install hooks', function(done) {
+          var async_spy = sinon.spy(hooks, 'post_install');
+
+          tasks.post_install({}, function(err) {
+            async_spy.called.should.be.false;
+            async_spy.restore();
+            done();
+          })
+        })
+
+      })
+
+      describe('if daemon.install works', function() {
+
+        before(function() {
+          stub = sinon.stub(daemon, 'install', function(cb) { cb() })
+        })
+
+        after(function() {
+          stub.restore();
+        })
+
+        describe('and post_install hooks fails', function() {
+
+          var hooks_stub;
+
+          before(function() {
+            hooks_stub = sinon.stub(hooks, 'post_install', function(cb) { cb(new Error('Not today')) })
+          })
+
+          after(function() {
+            hooks_stub.restore();
+          })
+
+          it('returns error', function(done) {
+            tasks.post_install({}, function(err) {
+              err.should.exist;
+              err.message.should.eql('Not today');
+              done();
+            })
+          })
+
+        })
+
+        describe('and post_install hooks succeed', function() {
+
+          var hooks_stub;
+
+          before(function() {
+            hooks_stub = sinon.stub(hooks, 'post_install', function(cb) { cb() })
+          })
+
+          after(function() {
+            hooks_stub.restore();
+          })
+
+          it('returns no error', function(done) {
+            tasks.post_install({}, function(err) {
+              should.not.exist(err);
+              done();
+            })
+          })
+
+        })
+
+      })
+
+    })
+
   })
 
   describe('pre_uninstall', function() {
@@ -398,8 +692,8 @@ describe('tasks', function() {
 
     describe('windows', function() {
 
-      var hooks    = require(helpers.lib_path('conf', 'tasks', 'os', 'windows')),
-          firewall = require('firewall');
+      var win_hooks = require(helpers.lib_path('conf', 'tasks', 'os', 'windows')),
+          firewall  = require('firewall');
 
       describe('post_activate', function() {
 
@@ -416,7 +710,7 @@ describe('tasks', function() {
           })
 
          it('doesnt fail', function(done) {
-            hooks.post_activate(function(err) {
+            win_hooks.post_activate(function(err) {
               should.not.exist(err);
               done()
             })
@@ -439,7 +733,7 @@ describe('tasks', function() {
           })
 
           it('doesnt return error', function(done) {
-            hooks.post_activate(function(err) {
+            win_hooks.post_activate(function(err) {
               should.not.exist(err);
               done()
             })
