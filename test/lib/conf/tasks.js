9b852c68e32163611c37a04bd89678d33a858349
Tomás Pollak <tomaspollak@gmail.com>
2014-08-25 20:54:41 -0700

    Compat fixes for tests in Windows.

--- test/lib/conf/tasks.js
+++ test/lib/conf/tasks.js
@@ -18,32 +18,62 @@ var prey_user = require(helpers.lib_path('conf', 'tasks', 'prey_user')),
     hooks   = require(helpers.lib_path('conf', 'tasks', 'os', os_name )),
     daemon  = require(helpers.lib_path('conf', 'tasks', 'daemon'));
 
+var firewall  = require('firewall');
+
 describe('tasks', function() {
+
+    var old_config,
+        old_versions_path;
+
+    before(function() {
+      // process.stdout.writable = false;
+
+      // store old config so we can reset it afterwards
+      old_config = common.config;
+
+      // disable version paths for these tests
+      old_versions_path = common.system.paths.versions;
+      common.system.paths.versions = null;
+    })
+
+    after(function() {
+      process.stdout.writable = true;
+
+      common.config = old_config;
+      common.system.paths.versions = old_versions_path;
+    })
+
+  if (os_name == 'windows') {
+
+    var firewall_stubs = {};
+
+    before(function() {
+      firewall_stubs.add = sinon.stub(firewall, 'add_rule', function(obj, cb) { cb() })
+      firewall_stubs.del = sinon.stub(firewall, 'remove_rule', function(obj, cb) { cb() })
+    })
+
+    after(function() {
+      firewall_stubs.add.restore()
+      firewall_stubs.del.restore();
+    })
+
+  }
 
   describe('activate', function() {
 
+/*
+    var os_hooks;
+
+    before(function() {
+      os_hooks = sinon.stub(hooks, 'post_activate', function(cb) { return cb() })
+    })
+
+    after(function() {
+      os_hooks.restore();
+    })
+*/
+
     describe('config', function() {
-
-      var old_config,
-          old_version_paths;
-
-      before(function() {
-        // process.stdout.writable = false;
-
-        // store old config so we can reset it afterwards
-        old_config = common.config;
-
-        // disable version paths for these tests
-        old_version_paths = common.system.paths.versions;
-        common.system.paths.versions = null;
-      })
-
-      after(function() {
-        process.stdout.writable = true;
-
-        common.config = old_config;
-        common.system.paths.versions = old_version_paths;
-      })
 
       var load_config = function(file) {
         return getset.load({ path: file, type: 'file' });
@@ -84,8 +114,6 @@ describe('tasks', function() {
         var dir  = join(tmpdir, 'writable'),
             file = join(dir, 'test.conf');
 
-        var post_activate_stub;
-
           before(function(done) {
             common.system.paths.versions = null;
 
@@ -98,20 +126,14 @@ describe('tasks', function() {
 
             fs.existsSync(dir).should.be.false;
 
-            post_activate_stub = sinon.stub(hooks, 'post_activate', function(cb) {
-              cb(new Error('post_activate hooks called'));
-            })
-
             // ok, now go
             tasks.activate({}, function(err) {
-              err.message.should.eql('post_activate hooks called');
+              should.not.exist(err);
               done();
             });
           })
 
           after(function(done) {
-            post_activate_stub.restore();
-
             fs.unlink(file, function(err) {
               if (err) return done(err);
               fs.rmdir(dir, done);
@@ -146,8 +168,13 @@ describe('tasks', function() {
 
         describe('and nonexisting file', function() {
 
-          beforeEach(function() {
+          before(function() {
             common.config = load_config(file);
+
+            if (fs.existsSync(file)) {
+              fs.unlinkSync(file);
+            }
+
             fs.existsSync(file).should.be.false;
           })
 
@@ -193,6 +220,7 @@ describe('tasks', function() {
         describe('and existing file', function() {
 
           before(function(done) {
+            common.config = load_config(file);
             fs.writeFile(file, 'foo = bar\n', done);
           })
 
@@ -323,7 +351,7 @@ describe('tasks', function() {
             it('fails miserably', function(done) {
               tasks.activate({}, function(err) {
                 should.exist(err);
-                err.message.should.match(/Path not found:.*\/versions\/2.3.4/);
+                err.message.should.match(/Path not found:.*versions.?2.3.4/);
                 done();
               })
             })
@@ -345,7 +373,7 @@ describe('tasks', function() {
               it('fails miserably', function(done) {
                 tasks.activate({}, function(err) {
                   should.exist(err);
-                  err.message.should.match(/Path not found:.*\/versions\/2.3.4/);
+                  err.message.should.match(/Path not found:.*versions.?2.3.4/);
                   done();
                 })
               })
@@ -354,8 +382,8 @@ describe('tasks', function() {
 
             describe('and specific version dir is found', function() {
 
-              var version_dir = dir + '/2.3.4';
-              var install_dir = tmpdir + '/install';
+              var version_dir = join(dir, '/2.3.4');
+              var install_dir = join(tmpdir, 'install');
 
               before(function(done) {
                 fs.mkdir(version_dir, function(err) {
@@ -373,18 +401,24 @@ describe('tasks', function() {
 
                 before(function(done) {
                   common.system.paths.install = install_dir;
-                  fs.mkdirSync(install_dir);
-                  fs.chmod(install_dir, '000', done)
+
+                  if (!fs.existsSync(install_dir))
+                    fs.mkdirSync(install_dir);
+
+                  fs.chmod(install_dir, '0000', done)
                 })
 
-                after(function(done) {
-                  fs.rmdir(install_dir, done);
+                after(function() {
+                  if (fs.existsSync(install_dir)) {
+                    fs.chmodSync(install_dir, '0750')
+                    fs.rmdirSync(install_dir);
+                  }
                 })
 
                 it('fails miserably', function(done) {
                   tasks.activate({}, function(err) {
                     should.exist(err);
-                    err.code.should.match(/EPERM|EISDIR/);
+                    err.code.should.match(/EPERM|EISDIR|EBUSY/);
                     done();
                   })
                 })
@@ -435,6 +469,8 @@ describe('tasks', function() {
   describe('post_install', function() {
 
     var spy, stub;
+
+    // stub hooks
 
     describe('stage one', function() {
 
@@ -553,69 +589,69 @@ describe('tasks', function() {
 
       })
 
-    })
+      describe('non windows', function() {
 
-    describe('non windows', function() {
-
-      var old_platform;
-
-      before(function() {
-        // force platform to darwin
-        old_platform = process.platform;
-        process.platform = 'darwin';
-      })
-
-      after(function() {
-        // revert platform
-        process.platform = old_platform;
-      })
-
-      describe('if prey_user.create fails', function() {
+        var old_platform;
 
         before(function() {
-          stub = sinon.stub(prey_user, 'create', function(cb) { cb(new Error('Gave up')) });
+          // force platform to darwin
+          old_platform = process.platform;
+          process.platform = 'darwin';
         })
 
         after(function() {
-          stub.restore();
+          // revert platform
+          process.platform = old_platform;
         })
 
-        it('does not advance to stage two', function(done) {
-          var async_spy = sinon.spy(async, 'series');
+        describe('if prey_user.create fails', function() {
 
-          tasks.post_install({}, function(err) {
-            async_spy.called.should.be.false;
-            async_spy.restore();
-            done();
+          before(function() {
+            stub = sinon.stub(prey_user, 'create', function(cb) { cb(new Error('Gave up')) });
           })
+
+          after(function() {
+            stub.restore();
+          })
+
+          it('does not advance to stage two', function(done) {
+            var async_spy = sinon.spy(async, 'series');
+
+            tasks.post_install({}, function(err) {
+              async_spy.called.should.be.false;
+              async_spy.restore();
+              done();
+            })
+          })
+
         })
+
+        describe('if prey_user.create succeeds', function() {
+
+          before(function() {
+            stub = sinon.stub(prey_user, 'create', function(cb) { cb() });
+          })
+
+          after(function() {
+            stub.restore();
+          })
+
+          it('advances to stage two', function(done) {
+            var async_stub = sinon.stub(async, 'series', function(fx_arr, cb) {
+              return cb(new Error('async.series called.'));
+            })
+
+            tasks.post_install({}, function(err) {
+              err.should.exist;
+              err.message.should.eql('async.series called.');
+              async_stub.restore();
+              done();
+            })
+          })
+
+        });
 
       })
-
-      describe('if prey_user.create succeeds', function() {
-
-        before(function() {
-          stub = sinon.stub(prey_user, 'create', function(cb) { cb() });
-        })
-
-        after(function() {
-          stub.restore();
-        })
-
-        it('advances to stage two', function(done) {
-          var async_stub = sinon.stub(async, 'series', function(fx_arr, cb) {
-            return cb(new Error('async.series called.'));
-          })
-
-          tasks.post_install({}, function(err) {
-            err.should.exist;
-            err.message.should.eql('async.series called.');
-            async_stub.restore();
-            done();
-          })
-        })
-
-      });
 
     })
 
@@ -624,12 +660,22 @@ describe('tasks', function() {
       var create_user_stub;
 
       before(function() {
-        // start by assuming that create user was ok
-        create_user_stub = sinon.stub(prey_user, 'create', function(cb) { cb() });
+        // let's assume we have a versions path and that config.sync works
+        // we're already testing the versions/no versions logic in .activate()
+        old_versions_path = common.system.paths.versions;
+        common.system.paths.versions = tmpdir + '/versions';
+
+        // if running under win32, skip the set_up_version logic 
+        if (process.platform == 'win32') {
+          stage_one_stub = sinon.stub(vm, 'set_current', function(ver, cb) { cb() });
+        } else {
+          // if running not on windows, skip create user part
+          stage_one_stub = sinon.stub(prey_user, 'create', function(cb) { cb() });
+        }
       })
 
       after(function() {
-        create_user_stub.restore();
+        stage_one_stub.restore();
       })
 
       describe('if daemon.install fails', function() {
@@ -729,8 +775,14 @@ describe('tasks', function() {
 
     describe('windows', function() {
 
-      var win_hooks = require(helpers.lib_path('conf', 'tasks', 'os', 'windows')),
-          firewall  = require('firewall');
+      var win_hooks = require(helpers.lib_path('conf', 'tasks', 'os', 'windows'));
+
+      // if we're actually running on windows, reset the FW stubs
+      if (os_name == 'windows') {
+        before(function() {
+          firewall_stubs.add.restore();
+        })
+      }
 
       describe('post_activate', function() {
 
