8dd273047e3c2b834e2480493928b1693caf522b
Tomás Pollak <tomaspollak@gmail.com>
2014-11-05 18:13:59 -0800

    Make OSX ZIP packages with ditto, to preserve file attributes.

--- tools/build.sh
+++ tools/build.sh
@@ -172,7 +172,12 @@ zip_file(){
   elif [ "$OS" = 'windows' ]; then
     zip -9 -r "$ZIP" "$FOLDER" -x \*.sh \*linux/* \*mac/* \*darwin/* 1> /dev/null
   elif [ "$OS" = 'mac' ]; then
-    zip -9 -r "$ZIP" "$FOLDER" -x \*.cmd \*.exe \*.dll \*windows/* \*linux/* 1> /dev/null
+    if [ -n "$is_mac" ]; then
+      ditto -v -c -k --zlibCompressionLevel 9 --rsrc --extattr --noqtn --keepParent "$FOLDER" "$ZIP"
+    else
+      echo "Warning: Not creating OSX packages as files would lose their extended attributes."
+      # zip -9 -r "$ZIP" "$FOLDER" -x \*.cmd \*.exe \*.dll \*windows/* \*linux/* 1> /dev/null
+    fi
   elif [ "$OS" = 'linux' ]; then
     zip -9 -r "$ZIP" "$FOLDER" -x \*.cmd \*.exe \*.dll \*windows/* \*mac/* \*darwin/* 1> /dev/null
   fi
