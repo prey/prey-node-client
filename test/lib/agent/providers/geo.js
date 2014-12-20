4aa6f02e870f7c556acdae7f10320c87e2f37d95
Tomás Pollak <tomaspollak@gmail.com>
2014-12-20 02:20:23 -0300

    Fixed require in geo spec.

--- test/lib/agent/providers/geo.js
+++ test/lib/agent/providers/geo.js
@@ -1,4 +1,4 @@
-var helpers  = require('./../../../../helpers'),
+var helpers  = require('./../../../helpers'),
     should   = require('should'),
     sinon    = require('sinon'),
     needle   = require('needle'),
