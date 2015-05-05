dc2ed70f25260e8bc6517e704a1ff3ed88adb43c
Mauricio Schneider <me@mauricioschneider.com>
2015-05-05 17:32:04 -0300

    Fix bin specs: fake log file no longer needed

--- test/bin_spec.js
+++ test/bin_spec.js
@@ -1,7 +1,6 @@
 var should        = require('should'),
     path          = require('path'),
     fs            = require('fs'),
-    os            = require('os'),
     exec          = require('child_process').exec,
     spawn         = require('child_process').spawn,
     is_windows    = process.platform == 'win32';
@@ -11,7 +10,6 @@ var exec_env      = process.env, // so we can override it later
     bin_path      = path.join(__dirname, '..', 'bin'),
     bin_prey      = path.join(bin_path, 'prey'),
     node_bin      = path.join(bin_path, 'node'),
-    fake_log_file = path.join(os.tmpDir(), 'fake_test_log_file.log'),
     local_present = fs.existsSync(node_bin);
 
 if (is_windows) {
@@ -40,22 +38,6 @@ function run_bin_prey(args, cb) {
   }, 1500);
 }
 
-/*
-
-function mask_bin_prey(){
-  var bin_prey_content = fs.readFileSync(bin_prey,'utf8');
-  fs.renameSync(bin_prey, bin_prey + '.tmp');
-  var fake_prey_content = bin_prey_content.replace('if (scr',
-    'var require=function(str){console.log(str);console.log(process.argv);return 0;}\nif (scr');
-  fs.writeFileSync(bin_prey, fake_prey_content, {mode: 0755});
-}
-
-function unmask_bin_prey(){
-  fs.renameSync(bin_prey + '.tmp', bin_prey);
-}
-
-*/
-
 describe('bin/prey', function(){
 
   before(function(done) {
@@ -77,19 +59,10 @@ describe('bin/prey', function(){
         });
       });
 
-      after(function(done){
-        fs.unlink(fake_log_file, done);
-      });
-
-      // we use a fake log file since when running via mocha
-      // the spawned process does not have a tty attached, so
-      // it assumes that it is running on background and prints
-      // any output to the default log file (/var/log/prey.log)
       it('uses local node binary', function(done){
-        run_bin_prey(['-l', fake_log_file, '-N'], function(code) {
+        run_bin_prey(['-N'], function(code, out, err) {
           code.should.equal(0);
-          var read_version = fs.readFileSync(fake_log_file, 'utf8');
-          read_version.should.containEql(node_versions.local);
+          out.should.containEql(node_versions.local);
           done();
         })
       });
@@ -107,7 +80,6 @@ describe('bin/prey', function(){
 
       it('calls lib/agent/cli.js', function(done){
         run_bin_prey(['-h'], function(code, out, err){
-          // out.should.containEql('spreads its wings');
           out.should.containEql('--logfile');
           done();
         });
@@ -134,34 +106,6 @@ describe('bin/prey', function(){
         });
       });
     });
-
-/*
-
-    describe('when called with `test` argument', function(){
-      it('calls mocha', function(done){
-        run_bin_prey(' test', function(err, out){
-          var out_command =
-            is_windows ? 'node_modules\\.bin\\_mocha'
-                       : 'node_modules/.bin/_mocha'
-          out.should.containEql(out_command);
-          done();
-        });
-      });
-
-      it('passes any other arguments too (eg. `--reporter nyan`)', function(done){
-        run_bin_prey(' test --reporter nyan', function(err, out){
-          var out_command =
-            is_windows  ? 'node_modules\\.bin\\_mocha'
-                        : 'node_modules/.bin/_mocha';
-          out.should.containEql(out_command);
-          out.should.containEql('--reporter');
-          out.should.containEql('nyan');
-          done();
-        });
-      });
-    });
-
-*/
 
     describe('when called with unknown argument', function(){
       it('returns unknown option', function(done){
