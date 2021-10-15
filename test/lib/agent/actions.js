42900a318b56d2bc53b5a8e43f57f22b5ca8b552
Javier Acuna <javo@preyhq.com>
2021-10-15 17:26:01 -0300

    Keep same actions behavior by name

--- test/lib/agent/actions.js
+++ test/lib/agent/actions.js
@@ -2,8 +2,6 @@ var helpers = require('./../../helpers'),
     should    = require('should'),
     sinon     = require('sinon'),
     actions = helpers.load('actions');
-    // hooks   = helpers.load('hooks');
-    // hooks   = require('./../../../lib/agent/hooks');
 
 describe('actions', function(){
 
@@ -16,58 +14,63 @@ describe('actions', function(){
       setTimeout(() => {
         actions.stop('1234-5678')
         done();
-      }, 1000)
+      }, 3000)
       
     })
 
     describe('when an action with same id arrives', () => {
       it('throws an error', (done) => {
-        // actions.start('9876-5432', 'alert', {message: 'hey!'})
         actions.start('9876-5432', 'alarm', {sound: 'modem'})
 
         setTimeout(() => {
-          // testear q no se llama el start alert!
-
-          // actions.start('9876-5432', 'alert', {message: 'hey!'}, (err) => {
           actions.start('9876-5432', 'alarm', {sound: 'modem'}, (err) => {
-            console.log("ERROR OE!!", err.message)
             should.exist(err);
-            
             err.message.should.containEql('Already running');
             
             actions.stop('9876-5432')
             done();
           })
-
-
-          // hooks.on('error', (err) => {
-          //   console.log("ERROR OE!!", err)
-          //   done();
-          // })
-          
         }, 2000)
       })
     })
 
     describe('when an action with different id but same name arrives', () => {
-
-      it('stops current action and run the new one', (done) => {
-        this.timeout(700);
-        // actions.start('1234-5678', 'alert', {message: 'hi!'})
+      it('throws an error', (done) => {
         actions.start('1234-5678', 'alarm', {sound: 'modem'})
+        actions.start('xxxx-yyyy', 'alert', {message: 'hey!'})
         
         setTimeout(() => {
-    
-          // actions.start('9876-5432', 'alert', {message: 'hey!'})
-          actions.start('9876-5432', 'alarm', {sound: 'alarm'})
-    
-          setTimeout(() => {
-            actions.stop('9876-5432');
-            done();
-          }, 3000)
-          
-        }, 3000)
+          actions.start('9876-5432', 'alarm', {sound: 'alarm'}, (err) => {
+            should.exist(err);
+            err.message.should.containEql('Already running');
+            
+            actions.stop('1234-5678');
+            
+            setTimeout(() => {
+              actions.stop('xxxx-yyyy');
+              done();
+            }, 2000)
+          })
+        }, 2000)
       })
+
+      // it('stops current action and run the new one', (done) => {
+      //   this.timeout(700);
+      //   // actions.start('1234-5678', 'alert', {message: 'hi!'})
+      //   actions.start('1234-5678', 'alarm', {sound: 'modem'})
+        
+      //   setTimeout(() => {
+    
+      //     // actions.start('9876-5432', 'alert', {message: 'hey!'})
+      //     actions.start('9876-5432', 'alarm', {sound: 'alarm'})
+    
+      //     setTimeout(() => {
+      //       actions.stop('9876-5432');
+      //       done();
+      //     }, 3000)
+          
+      //   }, 3000)
+      // })
     })
 
   })
