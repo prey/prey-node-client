c69d8d29f28106b22beffcd9a74fbee67c1e73b3
claudio torres aviles <claudio@preyhq.com>
2022-01-28 09:52:51 -0300

    delete openwebbrowser.sh from root directory

--- openwebbrowser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-webbrowser=`xdg-settings get default-web-browser`
-if [ "$webbrowser" == "firefox.desktop" ]; then
-    firefox -CreateProfile Prey
-    firefox -P Prey -url $1
-else
-    xdg-open $1
-fi
