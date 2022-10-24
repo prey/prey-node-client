ddc545d0b279a93f2e15e246132a2b821f635120
johaoRosasRosillo <johao@preyhq.com>
2022-10-24 11:10:29 -0300

    test comment

---
+++ test/lib/agent/providers/hardware.js
@@ -0,0 +1,323 @@
+// var join        = require('path').join,
+//     tmpdir      = require('os').tmpdir,
+//     helpers     = require('./../../../helpers'),
+//     sinon       = require('sinon'),
+//     should      = require('should'),
+//     lib_path    = helpers.lib_path(),
+//     storage     = require(join(lib_path, 'agent', 'utils', 'storage')),
+//     os          = require('os'),
+//     provider    = helpers.load('providers/hardware');
+
+// describe('hardware', function(){
+
+//   describe('first_mac_address', function() {
+
+//     it('should cb a valid mac address', function(done) {
+//       provider.get_first_mac_address(function(err, mac) {
+//         should.exist(mac);
+//         mac.should.have.lengthOf(17);
+//         done();
+//       });
+//     });
+
+//   });
+
+//   describe('network_interfaces_list', function() {
+
+//     it('should return at least 1 network interface',function(done) {
+//       provider.get_network_interfaces_list(function(err, nics) {
+
+//         should.not.exist(err);
+
+//         nics.should.be.an.Array;
+//         nics.length.should.be.above(0);
+
+//         var keys = ['name', 'type', 'ip_address', 'mac_address'];
+//         if (process.platform == 'win32')
+//           keys = keys.concat(['vendor', 'model']);
+
+//         var nic = nics[0];
+//         keys.forEach((key, index) => {
+//           nic.should.have.key(key);
+//           if (index == keys.length - 1) done();
+//         })
+//       });
+//     });
+//   });
+
+//   describe('get_firmware_info', function(){
+
+//     // in linux there's a scenario when we don't have access to dmidecode
+//     describe('with no access to system info', function() {
+
+//       it('returns error');
+
+//     });
+
+//     describe('with access to sys info', function() {
+
+//       it('works')
+
+//     })
+
+//   });
+
+//   describe('get_processor_info', function(){
+
+//     it('should callback an object with model, speed and cores',function(done) {
+
+//       provider.get_processor_info(function(err, obj) {
+//         should.not.exist(err);
+//         obj.should.have.keys('speed', 'model', 'cores');
+//         done();
+//       });
+
+//     });
+
+//   });
+
+
+//   describe('hardware_changes', () => {
+
+//     var stored_data = [{
+//       processor_info: {
+//       model: 'Intel(R) Core(TM) i5 CPU @ 1.80GHz', speed: 1800, cores: 4 },
+//       network_interfaces_list: [
+//         { name: 'en0', type: 'Wireless', ip_address: '10.10.0.00', mac_address: 'aa:11:1a:ab:ba:ee' },
+//         { name: 'en2', type: 'Other', ip_address: null, mac_address: 'dd:77:7a:ab:ba:ff' }
+//       ],
+//       ram_module_list: [
+//         { bank: 'Bank 0/DIMM0:', size: 4096, speed: 1600, vendor: 'Samsung Electronics, Inc.', memory_type: 'DDR3', serial_number: '-' },
+//         { bank: 'Bank 1/DIMM0:', size: 4096, speed: 1600, vendor: 'Samsung Electronics, Inc.', memory_type: 'DDR3', serial_number: '-' } ],
+//       firmware_info: {
+//         device_type: 'Laptop', model_name: 'MacBook Air', vendor_name: 'Apple', bios_vendor: 'Apple', bios_version: '184.0.0.0.0',
+//         mb_version: '2.27f2', serial_number: 'XXXXXXXXXXX', uuid: 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+//       }
+//     }]
+
+//     var dummy_data1 = {
+//       processor_info: {
+//       model: 'Intel(R) Core(TM) i5 CPU @ 1.80GHz', speed: 1800, cores: 4 },
+//       network_interfaces_list: [
+//         { name: 'en2', type: 'Other', ip_address: null, mac_address: 'dd:77:7a:ab:ba:ff' },
+//         { name: 'en0', type: 'Wireless', ip_address: '10.10.0.00', mac_address: 'aa:11:1a:ab:ba:ee' }
+//       ],
+//       ram_module_list: [
+//         { bank: 'Bank 1/DIMM0:', size: 4096, speed: 1600, vendor: 'Samsung Electronics, Inc.', memory_type: 'DDR3', serial_number: '-' },
+//         { bank: 'Bank 0/DIMM0:', size: 4096, speed: 1600, vendor: 'Samsung Electronics, Inc.', memory_type: 'DDR3', serial_number: '-' }],
+//       firmware_info: {
+//         device_type: 'Laptop', model_name: 'MacBook Air', vendor_name: 'Apple', bios_vendor: 'Apple', bios_version: '184.0.0.0.0',
+//         mb_version: '2.27f2', serial_number: 'XXXXXXXXXXX', uuid: 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+//       }
+//     }
+
+//     var dummy_data = {
+//       processor_info: {
+//       model: 'Intel(R) Core(TM) i5 CPU @ 1.80GHz', speed: 1800, cores: 4 },
+//       network_interfaces_list: [
+//         { name: 'en0', type: 'Wireless', ip_address: '10.10.0.00', mac_address: 'aa:11:1a:ab:ba:ee' },
+//         { name: 'en2', type: 'Other', ip_address: null, mac_address: 'dd:77:7a:ab:ba:ff' },
+//         { name: 'en1', type: 'Another', ip_address: null, mac_address: 'aa:bb:cc::dd:ee:ff' },
+//       ],
+//       ram_module_list: [
+//         { bank: 'Bank 0/DIMM0:', size: 4096, speed: 1600, vendor: 'Samsung Electronics, Inc.', memory_type: 'DDR3', serial_number: '-' },
+//         { bank: 'Bank 1/DIMM0:', size: 4096, speed: 1600, vendor: 'Samsung Electronics, Inc.', memory_type: 'DDR3', serial_number: '-' } ],
+//       firmware_info: {
+//         device_type: 'Laptop', model_name: 'MacBook Air', vendor_name: 'Apple', bios_vendor: 'Apple', bios_version: '184.0.0.0.0',
+//         mb_version: '2.27f2', serial_number: 'XXXXXXXXXXX', uuid: 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+//       }
+//     }
+
+//     var dummy_data2 = {
+//       processor_info: {
+//       model: 'Intel(R) Core(TM) i5 CPU @ 1.80GHz', speed: 1800, cores: 4 },
+//       network_interfaces_list: [
+//         { name: 'en0', type: 'Wireless', ip_address: '10.10.0.00', mac_address: 'aa:11:1a:ab:ba:ee' }
+//       ],
+//       ram_module_list: [
+//         { bank: 'Bank 0/DIMM0:', size: 4096, speed: 1600, vendor: 'Samsung Electronics, Inc.', memory_type: 'DDR3', serial_number: '-' },
+//         { bank: 'Bank 1/DIMM0:', size: 4096, speed: 1600, vendor: 'Samsung Electronics, Inc.', memory_type: 'DDR3', serial_number: '-' } ],
+//       firmware_info: {
+//         device_type: 'Laptop', model_name: 'MacBook Air', vendor_name: 'Apple', bios_vendor: 'Apple', bios_version: '184.0.0.0.0',
+//         mb_version: '2.27f2', serial_number: 'XXXXXXXXXXX', uuid: 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+//       }
+//     }
+
+//     var dummy_data3 = {
+//       processor_info: {
+//       model: 'Intel(R) Core(TM) i5 CPU @ 1.80GHz', speed: 1800, cores: 4 },
+//       network_interfaces_list: [
+//         { name: 'en0', type: 'Wireless', ip_address: '10.10.0.00', mac_address: 'aa:11:1a:ab:ba:ee' },
+//         { name: 'en2', type: 'No Other', ip_address: null, mac_address: 'dd:77:7a:ab:ba:ff' }
+//       ],
+//       ram_module_list: [
+//         { bank: 'Bank 0/DIMM0:', size: 4096, speed: 1600, vendor: 'Samsung Electronics, Inc.', memory_type: 'DDR3', serial_number: '-' },
+//         { bank: 'Bank 1/DIMM0:', size: 4096, speed: 1600, vendor: 'Samsung Electronics, Inc.', memory_type: 'DDR3', serial_number: '-' } ],
+//       firmware_info: {
+//         device_type: 'Laptop', model_name: 'MacBook Air', vendor_name: 'Apple', bios_vendor: 'Apple', bios_version: '184.0.0.0.0',
+//         mb_version: '2.27f2', serial_number: 'YYYYYYYYYYYY', uuid: 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+//       }
+//     }
+
+//     describe('when doesnt exists stored hardware', () => {
+//       var spy_del,
+//           spy_store;
+
+//       before((done) => {
+//         spy_store = sinon.spy(storage.storage_fns, 'set');
+//         spy_del = sinon.spy(storage.storage_fns, 'del');
+//         storage.init('keys', tmpdir() + '/hardware.db', done)
+//       })
+
+//       after((done) => {
+//         spy_del.restore();
+//         spy_store.restore();
+//         storage.erase(tmpdir() + '/hardware.db', done);
+//       })
+
+//       it('stores the data', (done) => {
+//         provider.track_hardware_changes(dummy_data1);
+//         setTimeout(() => {
+//           spy_store.calledOnce.should.be.true;
+//           spy_del.notCalled.should.be.true;
+
+//           storage.do('all', {type: 'keys'}, (err, rows) => {
+//             should.not.exist(err);
+//             rows.length.should.be.equal(1);
+//             rows[0].id.should.be.equal("hardware");
+//             done();
+//           })
+//         }, 500)
+//       })
+//     })
+
+//     describe('when exists stored hardware', () => {
+//       var spy_del,
+//           spy_store;
+
+//       describe('and the data is the same', () => {
+
+//         before((done) => {
+//           storage.init('keys', tmpdir() + '/hardware.db', () => {
+//             storage.do('set', {type: 'keys', id: 'hardware', data: {value: JSON.stringify(dummy_data1)}}, () => {
+//               spy_store = sinon.spy(storage.storage_fns, 'set');
+//               spy_del = sinon.spy(storage.storage_fns, 'del');
+//               done();
+//             })
+//           });
+//         })
+
+//         after((done) => {
+//           spy_store.restore();
+//           spy_del.restore();
+//           storage.erase(tmpdir() + '/hardware.db', done);
+//         })
+
+//         it('shouldnt edit the local database', (done) => {
+//           storage.do('all', {type: 'keys'}, (err, rows) => {
+//             provider.track_hardware_changes(dummy_data1);
+//             setTimeout(() => {
+//               spy_store.callCount.should.be.equal(0);
+//               spy_del.callCount.should.be.equal(0);
+//               done();
+//             }, 500);
+//           })
+//         })
+//       });
+
+//       describe('and the data is different', () => {
+//         var spy_del,
+//             spy_store;
+
+//         before((done) => {
+//           storage.init('keys', tmpdir() + '/hardware.db', () => {
+//             storage.do('set', {type: 'keys', id: 'hardware', data: {value: JSON.stringify(dummy_data1)}}, done)
+//           })
+//         })
+
+//         after((done) => {
+//           storage.erase(tmpdir() + '/hardware.db', done);
+//         });
+
+//         describe('when theres a new field', () => {
+//           before(() => {
+//             spy_store = sinon.spy(storage.storage_fns, 'set');
+//             spy_del = sinon.spy(storage.storage_fns, 'del');
+//           })
+
+//           after(() => {
+//             spy_store.restore();
+//             spy_del.restore();
+//           })
+
+//           it('replace the stored data', (done) => {
+//             provider.track_hardware_changes(dummy_data);
+//             setTimeout(() => {
+//               storage.do('all', {type: 'keys'}, (err, rows) => {
+//                 JSON.parse(rows[0].value).network_interfaces_list.length.should.be.equal(3);
+//                 spy_store.callCount.should.be.equal(1);
+//                 spy_del.callCount.should.be.equal(1);
+//                 done();
+//               });
+//             }, 500)
+//           })
+//         })
+
+//         describe('when a field is missing', () => {
+//           before(() => {
+//             spy_store = sinon.spy(storage.storage_fns, 'set');
+//             spy_del = sinon.spy(storage.storage_fns, 'del');
+//           })
+
+//           after(() => {
+//             spy_store.restore();
+//             spy_del.restore();
+//           })
+
+//           it('replace the stored data3', (done) => {
+//             provider.track_hardware_changes(dummy_data2);
+//             setTimeout(() => {
+//               storage.do('all', {type: 'keys'}, (err, rows) => {
+
+//                 //console.log("dummy data :",dummy_data2.network_interfaces_list)
+//                 //console.log("network data :",JSON.parse(rows[0].value).network_interfaces_list)
+//                 JSON.parse(rows[0].value).network_interfaces_list.length.should.be.equal(1);
+//                 spy_store.callCount.should.be.equal(1);
+//                 spy_del.callCount.should.be.equal(1);
+//                 done();
+//               });
+//             }, 500)
+//           })
+//         })
+
+//         describe('when a field changed', () => {
+//           before(() => {
+//             spy_store = sinon.spy(storage.storage_fns, 'set');
+//             spy_del = sinon.spy(storage.storage_fns, 'del');
+//           })
+
+//           after(() => {
+//             spy_store.restore();
+//             spy_del.restore();
+//           })
+
+//           it('replace the stored data', (done) => {
+//             provider.track_hardware_changes(dummy_data3);
+//             setTimeout(() => {
+//               storage.do('all', {type: 'keys'}, (err, rows) => {
+//                 var data = JSON.parse(rows[0].value);
+
+//                 data.network_interfaces_list.length.should.be.equal(2);
+//                 data.firmware_info.serial_number.should.be.equal('YYYYYYYYYYYY');
+//                 spy_store.callCount.should.be.equal(1);
+//                 spy_del.callCount.should.be.equal(1);
+//                 done();
+//               });
+//             }, 500)
+//           })
+//         })
+
+//       })
+//     })
+//   })
+// });
---
+++ test/lib/agent/updating_spec.js
@@ -0,0 +1,394 @@
+// var join          = require('path').join,
+//     sinon         = require('sinon'),
+//     should        = require('should'),
+//     child_process = require('child_process'),
+//     needle        = require('needle'),
+//     tmpdir        = require('os').tmpdir,
+//     helpers       = require(join('..', '..', 'helpers')),
+//     common        = require(helpers.lib_path('common')),
+//     system        = require(helpers.lib_path('system')),
+//     package       = require(helpers.lib_path('package')),
+//     geo           = require(helpers.lib_path('agent', 'providers', 'geo')),
+//     shared        = require(helpers.lib_path('conf', 'shared')),
+//     storage       = require(helpers.lib_path('agent', 'utils', 'storage')),
+//     updater       = require(helpers.lib_path('agent', 'updater'));
+
+// var versions_path = system.paths.versions,
+//     spy_logger;
+
+// var id = "8c0b5054-352e-4e77-941a-bfc07fcf53b1"
+
+// describe('updating', function() {
+
+//   var branch_stub;
+
+//   before(function() {
+//     common.logger.pause();
+
+//     // ensure the config.get('download_edge') call returns false
+//     branch_stub = sinon.stub(common.config, 'get').callsFake(key => {
+//       return false;
+//     })
+//   })
+
+//   after(function() {
+//     common.logger.resume();
+//     branch_stub.restore();
+//   });
+
+//   describe('when there is NO target', function() {
+
+//     before(function(){
+//       spy_logger = sinon.spy(updater.logger, 'warn');
+//     });
+
+//     after(function() {
+//       spy_logger.restore();
+//     })
+
+//     it('logs an error and returns', function(done) {
+//       updater.check();
+//       spy_logger.calledOnce.should.be.equal(true);
+//       spy_logger.getCall(0).args[0].should.containEql('No target for upgrade command found');
+//       done();
+//     });
+
+//   });
+
+//   describe('when there is a target', function() {
+
+//     describe('and there is NO versions support', function() {
+
+//       before(function(){
+//         system.paths.versions = undefined;
+//         spy_logger = sinon.spy(updater.logger, 'warn');
+//       });
+  
+//       after(function() {
+//         system.paths.versions = versions_path;
+//         spy_logger.restore();
+//       })
+  
+//       it('logs an error and returns', function(done) {
+//         updater.check(id, "yeah");
+//         spy_logger.calledOnce.should.be.equal(true);
+//         spy_logger.getCall(0).args[0].should.containEql('No versions support.');
+//         done();
+//       });
+  
+//     });
+
+//     describe('there is versions support', function() {
+//       before(function() {
+//         system.paths.versions = "/preypath/versions";
+//       });
+
+//       describe('and the target is invalid', function() {
+//         before(function() {
+//           spy_logger = sinon.spy(updater.logger, 'warn');
+//         });
+    
+//         after(function() {
+//           spy_logger.restore();
+//         })
+
+//         it('logs an invalidad target error', function(done) {
+//           updater.check(id, "im not valid");
+//           spy_logger.calledOnce.should.be.equal(true);
+//           spy_logger.getCall(0).args[0].should.containEql('Invalid target for upgrade command');
+//           done();
+//         });
+
+//       });
+
+//       describe('and the target is valid', function() {
+//         before(function() {
+//           spy_logger = sinon.spy(updater.logger, 'warn');
+//           system.paths.versions = "/preypath/versions";
+//         });
+
+//         after(() => {
+//           spy_logger.restore();
+//         })
+          
+//         it('returns activation error', (done) => {
+//           updater.check(id, "activate")
+//           spy_logger.calledOnce.should.be.equal(true);
+//           spy_logger.getCall(0).args[0].should.containEql('Missing client version to activate');
+//           done();
+//         })
+
+//       });
+
+//     });
+
+//   });
+
+//   describe('when there is versions support', function() {
+
+//     before(function() {
+//       system.paths.versions = '/somewhere/over/the/rainbow';
+//     });
+
+//     after(function() {
+//       system.paths.versions = versions_path;
+//     })
+
+//     describe('and no new versions are found', function() {
+
+//       var stub,
+//           real_version,
+//           upstream_version;
+
+//       before(function(done) {
+//         real_version = common.version;
+//         common.version = '1.2.3';
+//         upstream_version = '1.2.1'; // should not happen, but anyway
+
+//         stub = stub_get_stable_version(upstream_version);
+//         storage.init('versions', tmpdir() + '/version', done);
+//       });
+
+//       after(function(done) {
+//         common.version = real_version;
+//         stub.restore();
+//         storage.erase(tmpdir() + '/version', done);
+//       });
+
+//       it('callsback with no errors', function(done) {
+
+//         updater.check(id, 'reset', {}, function(err, ver) {
+//           should.exist(err);
+//           err.message.should.containEql('Theres no new version available');
+//           done();
+//         });
+
+//       })
+
+//     })
+
+//     describe('when a new version is available', function(){
+
+//       var stub,
+//           real_version,
+//           upstream_version;
+
+//       before(function() {
+//         real_version = common.version;
+//         common.version = '1.2.3';
+//         upstream_version = '1.2.5';
+
+//         stub = stub_get_stable_version(upstream_version);
+//       });
+
+//       after(function() {
+//         common.version = real_version;
+//         stub.restore();
+//       });
+
+//       // for this test, we fake the 'spawn' call and return a fake child,
+//       // for whom we will trigger a fake 'exit' event, as if the child process
+//       // had exited, so updater.check's callback gets triggered
+//       describe('when upgrading fails', function() {
+
+//         var fake_spawn;
+//         describe('by emits an exit', () => {
+
+//           before(function() {
+//             fake_spawn = sinon.stub(child_process, 'spawn').callsFake((cmd, args, opts) => {
+//               var child = helpers.fake_spawn_child();
+
+//               setTimeout(function(){
+//                 child.stdout.emit('data', Buffer.from('Downloading file...'));
+//                 child.stdout.emit('data', Buffer.from('Launching rockets'));
+//                 child.stdout.emit('data', Buffer.from('SHOOT!!'));
+//                 child.emit('exit');
+//               }, 10);
+
+//               return child;
+//             });
+//           })
+
+//           after(function() {
+//             fake_spawn.restore();
+//           });
+
+//           it('callbacks an error', function (done){
+
+//             updater.check(id, 'reset', {}, function(err, ver) {
+//               should.exist(err);
+//               err.message.should.equal('Upgrade to 1.2.5 failed. Exit code: undefined');
+//               err.stack.should.containEql('Launching rockets\nSHOOT!!');
+//               done();
+//             });
+
+//           });
+//         });
+
+//         describe('data contains error', () => {
+//           before(function() {
+//             updater.check_enabled = true;
+//             updater.upgrading = false;
+//             device_stub = sinon.stub(shared.keys, 'verify_current').callsFake(cb => { return cb(null); });
+//             fake_spawn = sinon.stub(child_process, 'spawn').callsFake((cmd, args, opts) => {
+//               var child = helpers.fake_spawn_child();
+
+//               setTimeout(function(){
+//                 child.stdout.emit('data', Buffer.from('Downloading file...'));
+//                 child.stdout.emit('data', Buffer.from('Launching rockets'));
+//                 child.stdout.emit('data', Buffer.from('Error! No, cant do'));
+//               }, 10);
+
+//               post_event_stub = sinon.spy(package, 'post_event');
+//               geo_loc_stub = sinon.stub(geo, 'fetch_location').callsFake((cb) => {
+//                 return cb(null, {location: {lat: null, lng: null}})
+//               });
+//               post_spy = sinon.stub(needle, 'post').callsFake((url, data, opts, cb) => {
+//                 return cb();
+//               });
+
+//               return child;
+//             });
+//           })
+
+//           after(function(done) {
+//             fake_spawn.restore();
+//             post_event_stub.restore();
+//             post_spy.restore();
+//             geo_loc_stub.restore();
+//             device_stub.restore();
+//             storage.erase(tmpdir() + '/versions', done);
+//           });
+
+//           // it('callbacks an error and notifies it', function (done){
+
+//            //   storage.init('versions', tmpdir() + '/versions', (err) => {
+//           //     storage.do('set', {type: 'versions', id: '1.2.5', data:  {from: '1.2.3', to: '1.2.5', attempts: 3, notified: 0}}, () => {
+//           //       done();
+//           //       updater.check_for_update();
+//           //       setTimeout(() => {
+
+
+//           //         post_spy.calledOnce.should.equal(true);
+//           //         done();
+//           //       }, 2500)
+
+//           //     });
+//           //   });
+
+//           // });
+//         });
+
+//       });
+
+//       // for this test, we fake the 'spawn' call and return a fake child,
+//       // for whom we will emit the 'YOUARENOTMYFATHER' string in its stdout
+//       // as if the updater is succesfully going through.
+//       describe('when upgrading succeeds', function() {
+
+//         var fake_spawn,
+//             fake_exit,
+//             exit_code,
+//             post_event_stub,
+//             post_spy,
+//             unreffed = false;
+
+//         before(function(done) {
+//           updater.check_enabled = true;
+//           updater.upgrading = false;
+//           device_stub = sinon.stub(shared.keys, 'verify_current').callsFake(cb => { return cb(null); });
+//           fake_spawn = sinon.stub(child_process, 'spawn').callsFake((cmd, args, opts) => {
+//             var child = helpers.fake_spawn_child();
+
+//             child.unref = function() {
+//               unreffed = true;
+//               child.emit('exit');
+//             }
+
+//             setTimeout(function() {
+//               child.stdout.emit('data', Buffer.from('YOUARENOTMYFATHER'));
+//             }, 100);
+
+//             return child;
+//           });
+
+//           fake_exit = sinon.stub(process, 'exit').callsFake(code => {
+//             exit_code = code;
+//           });
+
+//           post_event_stub = sinon.spy(package, 'post_event');
+//           geo_loc_stub = sinon.stub(geo, 'fetch_location').callsFake((cb) => {
+//             return cb(null, {location: {lat: null, lng: null}})
+//           });
+//           post_spy = sinon.stub(needle, 'post').callsFake((url, data, opts, cb) => {
+//             return cb();
+//           });
+
+//           storage.init('versions', tmpdir() + '/versions', done);
+//         });
+
+//         after(function(done) {
+//           fake_spawn.restore();
+//           fake_exit.restore();
+//           post_event_stub.restore();
+//           geo_loc_stub.restore();
+//           post_spy.restore();
+//           device_stub.restore();
+//           storage.erase(tmpdir() + '/versions', done);
+//         });
+
+//         it('process exits with status code(0)', function (done){
+//           this.timeout(16000);
+//           updater.check(id, 'reset', {}, function(err, ver) {
+//             exit_code.should.equal(0);
+//             unreffed.should.be.true;
+//             done();
+//           });
+
+//         });
+
+//         it('notifies update success when the client restarts after upgrade', (done) => {
+//           updater.check_enabled = true;
+//           updater.upgrading = false;
+//           common.version = '1.2.5';
+//           storage.do('set', {type: 'versions', id: 'version-1.2.5', data:  {from: '1.2.3', to: '1.2.5', attempts: 5, notified: 0}}, () => {
+//             updater.check_for_update(function(err) {
+//               should.exist(err);
+//               err.message.should.containEql('Theres no new version available');
+//               //post_spy.calledOnce.should.equal(true);//revisar
+//               storage.do('all', {type: 'versions'}, (err, out) => {
+//                 Object.keys(out).length.should.be.equal(0)
+//                 done();
+//               })
+//             });
+//           });
+//         })
+
+//       });
+
+//     });
+
+//   });
+
+//   function stub_get_stable_version(ver) {
+//     var fn = function(url, opts, cb) {
+//       if (typeof opts == 'function') {
+//         cb = opts;
+//         opts = {};
+//       }
+
+//       function resp(err, body, code) {
+//         cb(null, { statusCode: code || 200, body: body }, body);
+//       }
+
+//       if (url.match('latest.txt')) {
+//         resp(null, ver);
+//       } else {
+//         resp(new Error('GET ' + url));
+//       }
+//     };
+
+//     return sinon.stub(needle, 'get').callsFake(fn);
+//   }
+
+// });
