b350f4a9d2f27c8019c36ba0bb3deb9c07197d14
Pato Jofre <luis.jofre.g@gmail.com>
2023-06-19 20:55:08 +0100

    wip

--- tools/generate_config.js
+++ tools/generate_config.js
@@ -4,21 +4,11 @@
 // Dumps new config file in root_path/prey.conf.default
 ////////////////////////////////////////////////////////////
 
-var path    = require('path'),
-    getset  = require('getset'),
-    plugins = require('wink').init('./lib/agent/plugins'),
-    version = require(__dirname + '/../package').version;
+const getset = require('getset');
 
-var base_opts   = __dirname + '/../lib/agent/default.options',
-    config_file = __dirname + '/../prey.conf.default';
-
-var temp_config = getset.load(base_opts);
-
-function plugin_list() {
-  var sort_method = function(a,b) { return a != 'control-panel' && a > b };
-  var list = temp_config.get('plugin_list').sort(sort_method);
-  return list;
-}
+const baseOpts = __dirname + '/../lib/agent/default.options';
+const configFile = __dirname + '/../prey.conf.default';
+const tempConfig = getset.load(baseOpts);
 
 function is_empty(val) {
   return typeof val == 'undefined' || val === null;
@@ -36,32 +26,22 @@ function merge_options(options, plugin_name) {
   }
 
   // merge values
-  temp_config.set(plugin_name, values);
+  tempConfig.set(plugin_name, values);
 
   // now merge comments
   var obj = {};
   obj[plugin_name] = comments;
-  temp_config.merge_data('meta', { comments: obj }, true);
+  tempConfig.merge_data('meta', { comments: obj }, true);
 }
 
 function generate(destination) {
+  tempConfig.path = destination;
 
-  plugin_list().forEach(function(plugin_name) {
-    console.log('Getting options for ' + plugin_name)
-
-    var options = plugins.get(plugin_name).options;
-    if (options) {
-      merge_options(options, plugin_name);
-    }
-  })
-
-  temp_config.path = destination;
-
-  temp_config.save(function(err){
+  tempConfig.save(function(err){
     if (err) return console.log(err);
-    else console.log("Config file saved in " + config_file);
+    else console.log("Config file saved in " + configFile);
   });
 
 }
 
-generate(config_file);
+generate(configFile);
